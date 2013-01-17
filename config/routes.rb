78ae5516338424a5fc323b9808e48de72f9698f7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-01-17 10:51:33 -0500

    Adding sufia

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 CurateNd::Application.routes.draw do
   Blacklight.add_routes(self)
   HydraHead.add_routes(self)
+  Hydra::BatchEdit.add_routes(self)
+
 
   devise_for :users
+  mount Sufia::Engine => '/'
+
 
   root to: 'welcome#index'
 end
