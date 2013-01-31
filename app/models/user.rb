a35b777eb173699cb773e7bc8f0f55303e429298
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-01-31 12:00:26 -0500

    Updating config for batchuser and audituser
    Sufia has two system users (batchuser and audituser) that are used for
    the audit trail on background jobs. This needed to be configured
    according to our CAS setup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,15 +14,34 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :remember_me, :username
-  # attr_accessible :title, :body
+  attr_accessible :email, :remember_me, :username#, :password
 
   attr_accessor :password
+
+  def password_required?; false; end
+  def email_required?; false; end
+
+  def self.audituser
+    User.find_by_user_key(audituser_key) || User.create!(Devise.authentication_keys.first => audituser_key)
+  end
+
+  def self.audituser_key
+    'curate_nd_audituser'
+  end
+
+  def self.batchuser
+    User.find_by_user_key(batchuser_key) || User.create!(Devise.authentication_keys.first => batchuser_key)
+  end
+
+  def self.batchuser_key
+    'curate_nd_batchuser'
+  end
+
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account.
   def to_s
-    email
+    username
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.batchuser' do
+    it 'persists an instance the first time, then returns the persisted object' do
+      expect {
+        User.batchuser
+      }.to change { User.count }.by(1)
+
+      expect {
+        User.batchuser
+      }.to change { User.count }.by(0)
+    end
+  end
+
+  describe '.audituser' do
+    it 'persists an instance the first time, then returns the persisted object' do
+      expect {
+        User.audituser
+      }.to change { User.count }.by(1)
+
+      expect {
+        User.audituser
+      }.to change { User.count }.by(0)
+    end
+  end
 end
