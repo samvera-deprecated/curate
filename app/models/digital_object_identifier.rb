c8255583a6474d5a661e98b768774de31a787f37
rbalekai <rbalekai@nd.edu>
2013-02-05 17:12:45 -0500

    corrected indentation

--- app/models/digital_object_identifier.rb
+++ app/models/digital_object_identifier.rb
@@ -1,29 +1,29 @@
 class DigitalObjectIdentifier
 
-	def initialize(target, creator, title, publisher, publicationyear)
-		@target = target
-		@creator = creator
-		@title = title
-		@publisher = publisher
-		@publicationyear = publicationyear
-	end
+  def initialize(target, creator, title, publisher, publicationyear)
+    @target = target
+    @creator = creator
+    @title = title
+    @publisher = publisher
+    @publicationyear = publicationyear
+  end
 
   attr_reader :target, :creator, :title, :publisher, :publicationyear
 
-	def data
-		"_target: #{@target}\ndatacite.creator: #{@creator}\ndatacite.title: #{@title}\ndatacite.publisher: #{@publisher}\ndatacite.publicationyear: #{@publicationyear}\n"
-	end
+  def data
+    "_target: #{@target}\ndatacite.creator: #{@creator}\ndatacite.title: #{@title}\ndatacite.publisher: #{@publisher}\ndatacite.publicationyear: #{@publicationyear}\n"
+  end
 
-	def create_doi
-		response = RestClient.post DoiConfig.url, data, :content_type => 'text/plain'
-		return response
-	rescue => e
-		puts e.message
-		puts e.backtrace
-		return e.message
-	end
+  def create_doi
+    response = RestClient.post DoiConfig.url, data, :content_type => 'text/plain'
+    return response
+  rescue => e
+    puts e.message
+    puts e.backtrace
+    return e.message
+  end
 end
 
 =begin
-	DigitalObjectIdentifier.new("_target: https://fedorapprd.library.nd.edu:8443/fedora/get/RBSC-CURRENCY:671/", "datacite.creator: Comstock, Adam", "datacite.title: Notre Dame Currency Test", "datacite.publisher: University of Notre Dame - Libraries", "datacite.publicationyear: 2011")
+  DigitalObjectIdentifier.new("_target: https://fedorapprd.library.nd.edu:8443/fedora/get/RBSC-CURRENCY:671/", "datacite.creator: Comstock, Adam", "datacite.title: Notre Dame Currency Test", "datacite.publisher: University of Notre Dame - Libraries", "datacite.publicationyear: 2011")
 =end
