54dbe6b7a693846adb432742462b5f138538d3ee
rbalekai <rbalekai@nd.edu>
2013-02-06 12:09:52 -0500

    Test for digital_object_identifier

--- app/models/digital_object_identifier.rb
+++ app/models/digital_object_identifier.rb
@@ -1,5 +1,6 @@
 class DigitalObjectIdentifier
 
+=begin
   def initialize(target, creator, title, publisher, publicationyear)
     @target = target
     @creator = creator
@@ -7,8 +8,8 @@ class DigitalObjectIdentifier
     @publisher = publisher
     @publicationyear = publicationyear
   end
-
-  attr_reader :target, :creator, :title, :publisher, :publicationyear
+=end
+  attr_accessor :target, :creator, :title, :publisher, :publicationyear
 
   def data
     "_target: #{@target}\ndatacite.creator: #{@creator}\ndatacite.title: #{@title}\ndatacite.publisher: #{@publisher}\ndatacite.publicationyear: #{@publicationyear}\n"
@@ -18,12 +19,8 @@ class DigitalObjectIdentifier
     response = RestClient.post DoiConfig.url, data, :content_type => 'text/plain'
     return response
   rescue => e
-    puts e.message
+    puts e.inspect
     puts e.backtrace
-    return e.message
   end
 end
 
-=begin
-  DigitalObjectIdentifier.new("_target: https://fedorapprd.library.nd.edu:8443/fedora/get/RBSC-CURRENCY:671/", "datacite.creator: Comstock, Adam", "datacite.title: Notre Dame Currency Test", "datacite.publisher: University of Notre Dame - Libraries", "datacite.publicationyear: 2011")
-=end
---
+++ spec/models/digital_object_identifier_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe DigitalObjectIdentifier do
+
+  before :each do
+    @doi = DigitalObjectIdentifier.new
+  end
+
+  describe 'create_doi' do
+    it 'should_not_create_digital_object_identifier' do
+      response = @doi.create_doi
+      response.should == nil
+    end
+
+    it 'should_create_digital_object_identifier' do
+      @doi.target          = "https://fedorapprd.library.nd.edu:8443/fedora/get/RBSC-CURRENCY:671/"
+      @doi.title           = "Notre Dame Test"
+      @doi.creator         = "Comstock, Adam"
+      @doi.publisher       = "University of Notre Dame - Libraries"
+      @doi.publicationyear = "2011"
+      response = @doi.create_doi
+      response.should include("success: doi:10.5072/FK2")
+    end
+  end
+end
