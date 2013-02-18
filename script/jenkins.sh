8b1711c1567fd1e2e781cd94c7a660caaa05265b
Don Brower <dbrower@nd.edu>
2013-02-18 12:04:34 -0500

    Get jenkins script ready for testing

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# Invoked by the CurateND-Integration Jenkins project
+# Run the continuous integration tests
+
 /shared/ruby_prod/ruby/1.9.3/bin/bundle install --path="$WORKSPACE/vendor/bundle" --binstubs="$WORKSPACE/vendor/bundle/bin" --shebang '/shared/ruby_prod/ruby/1.9.3/bin/ruby' --deployment --gemfile="$WORKSPACE/Gemfile"
 cd $WORKSPACE
 $WORKSPACE/vendor/bundle/bin/rake db:migrate db:test:prepare
