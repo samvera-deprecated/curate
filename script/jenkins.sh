ca01e3e655aeff3f672cd8a5907aa3e0d15d6642
Don Brower <dbrower@nd.edu>
2013-02-18 12:12:42 -0500

    Add git to the path for jenkins ci

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -3,6 +3,9 @@
 # Invoked by the CurateND-Integration Jenkins project
 # Run the continuous integration tests
 
+# git path
+export PATH=/shared/git/bin:$PATH
+
 /shared/ruby_prod/ruby/1.9.3/bin/bundle install --path="$WORKSPACE/vendor/bundle" --binstubs="$WORKSPACE/vendor/bundle/bin" --shebang '/shared/ruby_prod/ruby/1.9.3/bin/ruby' --deployment --gemfile="$WORKSPACE/Gemfile"
 cd $WORKSPACE
 $WORKSPACE/vendor/bundle/bin/rake db:migrate db:test:prepare
