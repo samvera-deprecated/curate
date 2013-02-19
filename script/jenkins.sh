02161a8922a0b94b8b9e4b26de753eb64beb89d2
Don Brower <dbrower@nd.edu>
2013-02-19 15:44:07 -0500

    Use secret_ci repo for ci testing

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -8,4 +8,14 @@ export PATH=/shared/git/bin:$PATH
 
 /shared/ruby_prod/ruby/1.9.3/bin/bundle install --path="$WORKSPACE/vendor/bundle" --binstubs="$WORKSPACE/vendor/bundle/bin" --shebang '/shared/ruby_prod/ruby/1.9.3/bin/ruby' --deployment --gemfile="$WORKSPACE/Gemfile"
 cd $WORKSPACE
+
+if [ -d secret_ci ];
+    rm -rf secret_ci
+fi
+git clone git@git.library.nd.edu:secret_ci
+
+for f in database.yml solr.yml fedora.yml; do
+    cp secret_ci/curate_nd/$f config/$f
+done
+
 $WORKSPACE/vendor/bundle/bin/rake db:migrate db:test:prepare
