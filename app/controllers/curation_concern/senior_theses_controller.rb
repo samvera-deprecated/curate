f9dd94adb1e144741df486e53b212f0ea0de0185
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-20 12:00:54 -0500

    Quick driving does the dashboard render
    It does. Now I need some UI-time

--- app/controllers/curation_concern/senior_theses_controller.rb
+++ app/controllers/curation_concern/senior_theses_controller.rb
@@ -12,7 +12,7 @@ class CurationConcern::SeniorThesesController < ApplicationController
   # Catch permission errors
   rescue_from Hydra::AccessDenied, CanCan::AccessDenied do |exception|
     if (exception.action == :edit)
-      redirect_to(sufia.url_for({:action=>'show'}), :alert => "You do not have sufficient privileges to edit this document")
+      redirect_to(url_for({:action=>'show'}), :alert => "You do not have sufficient privileges to edit this document")
     elsif current_user and current_user.persisted?
       redirect_to root_url, :alert => exception.message
     else
