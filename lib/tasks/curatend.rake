dadec930a39a139ffa4a39a6f79d6a406ecc5f35
Don Brower <dbrower@nd.edu>
2013-02-20 10:26:09 -0500

    Make curatend:ci task, adjust jenkins script to call it

---
+++ lib/tasks/curatend.rake
@@ -0,0 +1,18 @@
+#
+# Tasks specific to Curate ND
+#
+
+namespace :curatend do
+  desc "Execute Continuous Integration build (docs, tests with coverage)"
+  task :ci => :environment do
+    #Rake::Task["hyhead:doc"].invoke
+    Rake::Task["jetty:config"].invoke
+    #Rake::Task["db:drop"].invoke
+    #Rake::Task["db:create"].invoke
+    Rake::Task["db:migrate"].invoke
+
+    Rake::Task['spec'].invoke
+    Rake::Task['cucumber:ok'].invoke
+    #raise "test failures: #{error}" if error
+  end
+end
--- script/jenkins.sh
+++ script/jenkins.sh
@@ -2,22 +2,40 @@
 
 # Invoked by the CurateND-Integration Jenkins project
 # Run the continuous integration tests
+#
+# called from Jenkins command
+#       Build -> Execute Shell Command ==
+#       test -x $WORKSPACE/script/jenkins_build.sh && $WORKSPACE/script/jenkins_build.sh
+echo "=-=-=-=-=-=-=-= start $0"
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
 
-# git path
 export PATH=/shared/git/bin:$PATH
 
+echo "=-=-=-=-=-=-=-= bundle install"
 /shared/ruby_prod/ruby/1.9.3/bin/bundle install --path="$WORKSPACE/vendor/bundle" --binstubs="$WORKSPACE/vendor/bundle/bin" --shebang '/shared/ruby_prod/ruby/1.9.3/bin/ruby' --deployment --gemfile="$WORKSPACE/Gemfile"
+
+echo "=-=-=-=-=-=-=-= cd $WORKSPACE"
 cd $WORKSPACE
 
 if [ -d secret_ci ]; then
+    echo "=-=-=-=-=-=-=-= delete secret_ci"
     rm -rf secret_ci
 fi
+echo "=-=-=-=-=-=-=-= git clone secret_ci"
 git clone git@git.library.nd.edu:secret_ci
 
 for f in database.yml solr.yml fedora.yml; do
+    echo "=-=-=-=-=-=-=-= copy $f"
     cp secret_ci/curate_nd/$f config/$f
 done
 
 export RAILS_ENV=test
+export HEADLESS=true
 
-$WORKSPACE/vendor/bundle/bin/rake db:migrate db:test:prepare
+echo "=-=-=-=-=-=-=-= rake curatend:ci"
+$WORKSPACE/vendor/bundle/bin/rake --trace curatend:ci
+retval=$?
+
+echo "=-=-=-=-=-=-=-= $0 finished $retval"
+exit $retval
