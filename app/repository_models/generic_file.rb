2adaaa9a2b70ac72ab6280ede73ecaf28cccada3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-21 13:38:27 -0500

    Adding GenericFile#to_s

--- app/repository_models/generic_file.rb
+++ app/repository_models/generic_file.rb
@@ -1,4 +1,7 @@
 require Sufia::Engine.root.join('app/models/generic_file')
 class GenericFile
   belongs_to :batch, property: :is_part_of, class_name: 'ActiveFedora::Base'
+  def to_s
+    label
+  end
 end
