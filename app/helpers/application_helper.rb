3da7c13ca52c0cff139a6e1807752b9bad9af096
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-22 14:15:37 -0500

    Spoofing #sufia to act as if Sufia is mounted
    This is included to hopefully catch most of the sufia method calls that
    are vestigal for the Sufia engine being included in the Gemfile but
    unmounted.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 module ApplicationHelper
-  # Override to remove the label class (easier integration with bootstrap)
-  # and handles arrays
-  def render_facet_value_from_sufia(facet_solr_field, item, options ={})
-    if item.is_a? Array
-      render_array_facet_value(facet_solr_field, item, options)
-    end
-    path =url_for(add_facet_params_and_redirect(facet_solr_field, item.value).merge(:only_path=>true))
-    (link_to_unless(options[:suppress_link], item.value, path, :class=>"facet_select") + " " + render_facet_count(item.hits)).html_safe
+  # This is included to hopefully catch most of the sufia method calls that are
+  # vestigal for the Sufia engine being included in the Gemfile but unmounted.
+  def sufia
+    self
   end
-
 end
