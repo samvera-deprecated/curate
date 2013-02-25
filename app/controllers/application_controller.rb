da8ee343853bb1430139ad1112c753834b8ad7c6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-25 14:50:39 -0500

    Updating white spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
-   include Blacklight::Controller
-# Adds Hydra behaviors into the application controller
-  include Hydra::Controller::ControllerBehavior  
-# Adds Sufia behaviors into the application controller 
+  include Blacklight::Controller
+  # Adds Hydra behaviors into the application controller
+  include Hydra::Controller::ControllerBehavior
+  # Adds Sufia behaviors into the application controller
   include Sufia::Controller
 
 
