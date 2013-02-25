abb0f764f57a996a97cb82fd77a589598e9cb8c5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-25 14:26:52 -0500

    Adding multi-value form helper

---
+++ app/inputs/multi_value_input.rb
@@ -0,0 +1,30 @@
+class MultiValueInput < SimpleForm::Inputs::CollectionInput
+  def input
+    input_html_classes.unshift("string")
+    input_html_options[:type] ||= 'string' if html5?
+    input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
+    counter = 1
+    text = "<ul>\n"
+
+    collection.each do |value|
+      if value.to_s.strip.any?
+        input_html_options[:value] = value
+        input_html_options[:id] = "#{object_name}_#{attribute_name}_#{counter}"
+        counter += 1
+        text << "<li>#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
+      end
+    end
+    input_html_options[:value] = ''
+    input_html_options[:id] = "#{object_name}_#{attribute_name}_#{counter}"
+    text << "<li>#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
+    text << "</ul>\n"
+  end
+
+  protected
+  def collection
+    @collection ||= begin
+      object.send(attribute_name)
+    end
+  end
+  def multiple?; true; end
+end
