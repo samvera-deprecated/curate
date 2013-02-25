d6beb6b7de91c84c9bda6a96970a647bf05a4d26
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-02-25 15:44:17 -0500

    Helper method for navigation elements; tabindex adjustment
    link_to_unless would work except that the "disabled" class is required
    by bootstrap on the _containing_ element
    
    Messing with tabindex to _hopefully_ improve accessibility

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
   def sufia
     self
   end
+
+  def bootstrap_navigation_element(name, path)
+    markup = ""
+
+    if current_page? path
+      markup = <<HTML
+      <li class="disabled">
+        #{link_to name, '#', tabindex: :'-1'}
+      </li>
+HTML
+    else
+      markup = <<HTML
+      <li>
+        #{link_to name, path}
+      </li>
+HTML
+    end
+
+    markup.html_safe()
+  end
 end
