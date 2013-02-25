d498250b8492b7c763b7c8e702040d1fd698d8da
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-25 15:03:59 -0500

    Adding Capybara to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
+require 'capybara/rspec'
 
 
 # Requires supporting ruby files with custom matchers and macros, etc,
