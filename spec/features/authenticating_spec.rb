ecbc1d7d74804d67bb235cfc2b8cce094ec457f9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-26 11:19:22 -0500

    Creating some end to end tests to verify behavior
    I'm creating a process that will verify the following in order:
    * View homepage
    * Login
    * Select a Curation Concern
    * Create a Senior Thesis
    * Show the Senior Thesis
    * Edit the Senior Thesis
    * Update the Senior Thesis
    * View the dashboard
    * Search the dashboard

--- spec/features/authenticating_spec.rb
+++ spec/features/authenticating_spec.rb
@@ -23,20 +23,107 @@ describe 'welcome page authentication', type: :feature do
   end
   describe 'with user who has _not_ agreed to the terms of service' do
     let(:agreed_to_terms_of_service) { false }
+    let(:prefix) { Time.now.strftime("%Y-%m-%d-%H-%M-%S-%L") }
+    let(:initial_title) { "#{prefix} Something Special" }
+    let(:initial_file_path) { __FILE__ }
+    let(:updated_title) { "#{prefix} Another Not Quite" }
+    let(:updated_file_path) { Rails.root.join('app/controllers/application_controller.rb').to_s }
     it "displays the terms of service page after authentication" do
       login_as(user, scope: :user, run_callbacks: false)
+
+      get_started
+      agree_to_terms_of_service
+      classify_what_you_are_uploading
+      describe_your_thesis
+      view_your_new_thesis
+      edit_your_thesis
+      view_your_updated_thesis
+      view_your_dashboard
+    end
+    protected
+    def get_started
       visit '/'
       click_link "Get Started"
-      page.should_not have_content("What are you uploading?")
+    end
+
+    def agree_to_terms_of_service
       within('#terms_of_service') do
         click_on("I Agree")
       end
+    end
+
+    def classify_what_you_are_uploading
       page.should have_content("What are you uploading?")
       within('#new_classify') do
         select('Senior Thesis', from: 'classify_curation_concern')
         click_on("Continue")
       end
+    end
+    def describe_your_thesis
       page.should have_content('Describe Your Thesis')
+      within('#new_senior_thesis') do
+        fill_in("Title", with: initial_title)
+        attach_file("Upload your thesis", initial_file_path)
+        click_on("Create Senior thesis")
+      end
+    end
+
+    def view_your_new_thesis
+      page.should have_content("Thesis Details")
+      within(".senior_thesis.attributes") do
+        page.should have_content("Title")
+        page.should have_content(initial_title)
+      end
+      page.should have_content("File Details")
+      page.should have_content(File.basename(initial_file_path))
+    end
+
+    def edit_your_thesis
+      click_on("Edit Senior Thesis")
+      within('.edit_senior_thesis') do
+        fill_in("Title", with: updated_title)
+        page.should have_content("Current thesis file:")
+        page.should have_content(File.basename(initial_file_path))
+        attach_file("Upload a new copy of your thesis", updated_file_path)
+        click_on("Update Senior thesis")
+      end
+    end
+    def view_your_updated_thesis
+      page.should have_content("Thesis Details")
+      within(".senior_thesis.attributes") do
+        page.should have_content("Title")
+        page.should have_content(updated_title)
+      end
+      page.should have_content("File Details")
+      page.should have_content(File.basename(updated_file_path))
+      click_on("Back to Dashboard")
+    end
+
+    def view_your_dashboard
+      search_term = "\"#{updated_title}\""
+      within(".form-search") do
+        fill_in("q", with: search_term)
+        click_on("Go")
+      end
+      page.should have_content(updated_title)
+      within('.alert.alert-info') do
+        page.should have_content("You searched for: #{search_term}")
+      end
+
+      within('#facets') do
+        # I call CSS/Dom shenannigans; I can't access 'Creator' link
+        # directly and instead must find by CSS selector, validate it
+        creator_facet = find('a.accordion-toggle')
+        creator_facet.text.should == 'Creator'
+        creator_facet.click
+        click_on(user.username)
+      end
+      within('.alert.alert-info') do
+        page.should have_content("You searched for: #{search_term}")
+      end
+      within('.alert.alert-warning') do
+        page.should have_content(user.username)
+      end
     end
   end
 end
