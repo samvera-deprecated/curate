23e65c22074286ecf78da2af32bd5f49235d07c8
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-02-26 13:29:37 -0500

    @WIP primitive repeating field management in place

--- app/assets/javascripts/repeating_fields.js
+++ app/assets/javascripts/repeating_fields.js
@@ -1,62 +1,52 @@
 $(function() {
-  $.widget( "custom.manage_fields", {
+  $.widget( "curate.manage_fields", {
     options: {
-      onAdd: null,
-      onRemove: null
+      change: null,
+      add: null,
+      remove: null
     },
+
     _create: function() {
-      this.element.addClass( "managed" );
+      this.element.addClass("managed");
 
-      this.remover = $( "<button class=\"btn btn-danger remove\"><i class=\"icon-white icon-minus\"></i>Remove</button>");
-      this.adder   = $( "<button class=\"btn btn-success add\"><i class=\"icon-white icon-plus\"></i>Add</button>");
+      this.actions = $("<span class=\"field-actions\">");
+      this.remover = $("<button class=\"btn btn-danger remove\"><i class=\"icon-white icon-minus\"></i>Remove</button>");
+      this.adder   = $("<button class=\"btn btn-success add\"><i class=\"icon-white icon-plus\"></i>Add</button>");
 
-      $('.field-wrapper', this.element).append(this.remover);
-      $('.field-wrapper:last', this.element).append(this.adder);
+      $('.field-wrapper', this.element).append(this.actions);
+      $('.field-actions', this.element).append(this.remover);
+      $('.field-actions:last', this.element).append(this.adder);
 
-      this._on( this.remover, {
-        click: "remove_from_list"
+      console.log('** Create **');
+      this._on( this.element, {
+        "click .remove": "remove_from_list",
+        "click .add": "add_to_list"
       });
-
-      this._on( this.adder, {
-        click: "add_to_list"
-      });
-
-      this._refresh();
-    },
-    // called when created, and later when changing options
-    _refresh: function() {
-      this._trigger( "change" );
-    },
-
-    remove_from_list: function( event ) {
-      event.preventDefault();
-      console.log("Remover CLICK");
-      var $trigger = this._trigger
-      console.log($trigger);
     },
 
     add_to_list: function( event ) {
       event.preventDefault();
-      console.log("Adder CLICK");
-      var $trigger = this._trigger
-      console.log($trigger);
+      console.log('** Add **');
+      $(event.target)
+        .parents('.field-wrapper')
+        .clone()
+        .appendTo(this.element);
+      this._trigger("remove");
     },
-    // events bound via _on are removed automatically
-    // revert other modifications here
+
+    remove_from_list: function( event ) {
+      event.preventDefault();
+      console.log('** Remove **');
+      $(event.target)
+        .parents('.field-wrapper')
+        .remove();
+      this._trigger("add");
+    },
+
     _destroy: function() {
-      // remove generated elements
-      this.remover.remove();
-      this.adder.remove();
-      this.element
-        .removeClass( "managed" );
+      this.actions.remove();
+      this.element.removeClass( "managed" );
     },
-    // _setOptions is called with a hash of all options that are changing
-    // always refresh when changing options
-    _setOptions: function() {
-      // _super and _superApply handle keeping the right this-context
-      this._superApply( arguments );
-      this._refresh();
-    }
   });
 
   $('.multi_value.control-group').manage_fields();
