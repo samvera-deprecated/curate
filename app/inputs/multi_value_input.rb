2e63b5c1c0e5cbd34f9b9ea9cfd5a121f1904a9a
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-02-26 08:59:58 -0500

    Pass more information to the view for multi-value inputs

--- app/inputs/multi_value_input.rb
+++ app/inputs/multi_value_input.rb
@@ -4,19 +4,19 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
     input_html_options[:type] ||= 'string' if html5?
     input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
     counter = 1
-    text = "<ul>\n"
+    text = "<ul class=\"multi-value listing\">\n"
 
     collection.each do |value|
       unless value.to_s.strip.blank?
         input_html_options[:value] = value
         input_html_options[:id] = "#{object_name}_#{attribute_name}_#{counter}"
+        text << "<li class=\"field-wrapper\" data-attribute-name=\"#{object_name}_#{attribute_name}\" data-counter=\"#{counter}\">#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
         counter += 1
-        text << "<li>#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
       end
     end
     input_html_options[:value] = ''
     input_html_options[:id] = "#{object_name}_#{attribute_name}_#{counter}"
-    text << "<li>#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
+    text << "<li class=\"field-wrapper\" data-attribute-name=\"#{object_name}_#{attribute_name}\"data-counter=\"#{counter}\">#{@builder.text_field(attribute_name, input_html_options)}</li>\n"
     text << "</ul>\n"
   end
 
