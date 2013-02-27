d39d84fe6c434af345b54b7634b8a525139caf07
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 16:41:13 -0500

    Removing focus: true from specs
    Let them all run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
