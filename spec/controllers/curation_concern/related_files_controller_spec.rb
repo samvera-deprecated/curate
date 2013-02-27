2a3c159d9f06f6c14b40ee0c2253c66aed4cab97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 09:52:57 -0500

    Adding #parent_curation_concern for RelatedFiles

--- spec/controllers/curation_concern/related_files_controller_spec.rb
+++ spec/controllers/curation_concern/related_files_controller_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe CurationConcern::RelatedFilesController do
-  describe '#index' do
-    it 'requires a curation concern' do
-    end
+  before(:each) do
+    sign_in(user)
   end
+  let(:parent_curation_concern) {
+    FactoryGirl.create_curation_concern(:senior_thesis, user)
+  }
+  let(:user) { FactoryGirl.create(:user) }
+  it 'has a #parent_curation_concern' do
+    controller.params[:parent_curation_concern_id] = parent_curation_concern.noid
+    controller.parent_curation_concern.should == parent_curation_concern
+  end
+
 end
