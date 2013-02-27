6c38e9976ab1b611686f21a17e7d90c2929df915
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 16:52:54 -0500

    Adding preliminary test for related files

--- app/controllers/curation_concern/related_files_controller.rb
+++ app/controllers/curation_concern/related_files_controller.rb
@@ -1,4 +1,17 @@
-class CurationConcern::RelatedFilesController < ApplicationController
+class CurationConcern::RelatedFilesController < CurationConcern::BaseController
+  respond_to(:html)
+  before_filter :parent_curation_concern
+  load_and_authorize_resource :parent_curation_concern, class: "ActiveFedora::Base"
+
+  def curation_concern
+    @curation_concern ||=
+    if params[:id]
+      GenericFile.find(params[:id])
+    else
+      GenericFile.new(params[:generic_file])
+    end
+  end
+
   def parent_curation_concern
     @parent_curation_concern ||= ActiveFedora::Base.find(
       namespaced_parent_curation_concern_id,
@@ -11,4 +24,7 @@ class CurationConcern::RelatedFilesController < ApplicationController
     Sufia::Noid.namespaceize(params[:parent_curation_concern_id])
   end
   protected :namespaced_parent_curation_concern_id
+
+  def new
+  end
 end
--- spec/controllers/curation_concern/related_files_controller_spec.rb
+++ spec/controllers/curation_concern/related_files_controller_spec.rb
@@ -1,16 +1,36 @@
 require 'spec_helper'
 
 describe CurationConcern::RelatedFilesController do
-  before(:each) do
-    sign_in(user)
-  end
   let(:parent_curation_concern) {
     FactoryGirl.create_curation_concern(:senior_thesis, user)
   }
+  subject { FactoryGirl.create_generic_file(parent_curation_concern, user)}
   let(:user) { FactoryGirl.create(:user) }
+  let(:another_user)  { FactoryGirl.create(:user) }
   it 'has a #parent_curation_concern' do
     controller.params[:parent_curation_concern_id] = parent_curation_concern.noid
     controller.parent_curation_concern.should == parent_curation_concern
+  end
+
+  it 'has a #curation_concern' do
+    controller.curation_concern.should be_an_instance_of(GenericFile)
+  end
+
+  describe '#new' do
+    it 'renders a form if you can edit the parent' do
+      sign_in(user)
+      parent_curation_concern
+      get :new, parent_curation_concern_id: parent_curation_concern.to_param
+      expect(response).to render_template('new')
+    end
+
+    it 'renders a form if you can edit the parent' do
+      sign_in(another_user)
+      parent_curation_concern
+      get :new, parent_curation_concern_id: parent_curation_concern.to_param
+      response.status.should == 302
+      expect(response).to redirect_to(root_url)
+    end
   end
 
 end
