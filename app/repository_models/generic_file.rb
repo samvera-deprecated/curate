e24dc17e38bdd5ebfc755a0c21cfdf50cd77799b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 16:40:26 -0500

    Removing delegate_to as that is part of sufia
    I updated our existing Sufia branch to set the title to unique (instead
    of array).

--- app/repository_models/generic_file.rb
+++ app/repository_models/generic_file.rb
@@ -2,14 +2,6 @@ require Sufia::Engine.root.join('app/models/generic_file')
 class GenericFile
   belongs_to :batch, property: :is_part_of, class_name: 'ActiveFedora::Base'
 
-  delegate_to(
-      :descMetadata,
-      [
-          :title,
-      ],
-      unique: true
-  )
-
   attr_accessor :revised_thesis_file, :version
   def to_s
     label
