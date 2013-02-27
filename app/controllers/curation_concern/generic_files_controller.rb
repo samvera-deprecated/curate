b8f1c029fcdc0b2acf829e0c1a6d2b577dd360a2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 16:43:09 -0500

    Burning instance variables with fire
    Instance variables

--- app/controllers/curation_concern/generic_files_controller.rb
+++ app/controllers/curation_concern/generic_files_controller.rb
@@ -9,19 +9,16 @@ class CurationConcern::GenericFilesController < CurationConcern::BaseController
   end
 
   def edit
-    @curation_concern = GenericFile.find(params[:id])
-    @terms = @curation_concern.terms_for_editing
-    respond_with(@curation_concern, @terms)
+    respond_with(curation_concern)
   end
 
   def update
-    @curation_concern = GenericFile.find(params[:id])
-    CurationConcern::Actions.update_metadata(@curation_concern, current_user, params[:generic_file])
-    CurationConcern::Actions.update_file(@curation_concern, current_user, params[:generic_file])
-    CurationConcern::Actions.update_version(@curation_concern, current_user, params[:generic_file])
-    respond_with([:curation_concern, @curation_concern])
+    CurationConcern::Actions.update_metadata(curation_concern, current_user, params[:generic_file])
+    CurationConcern::Actions.update_file(curation_concern, current_user, params[:generic_file])
+    CurationConcern::Actions.update_version(curation_concern, current_user, params[:generic_file])
+    respond_with([:curation_concern, curation_concern])
   rescue ActiveFedora::RecordInvalid
-    respond_with([:curation_concern, @curation_concern]) do |wants|
+    respond_with([:curation_concern, curation_concern]) do |wants|
       wants.html { render 'edit', status: :unprocessable_entity }
     end
   end
