aff8f6025904183df5d02527f2ec2103fa4be26f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-27 16:41:45 -0500

    Improving FactoryGirl.create_curation_concern
    I want to allow an already created concern to be used, instead of
    creating a whole new one.

--- spec/factories/create_generic_file_factory.rb
+++ spec/factories/create_generic_file_factory.rb
@@ -1,5 +1,11 @@
-def FactoryGirl.create_generic_file(container_factory_name, user, file = nil)
-  curation_concern = FactoryGirl.create_curation_concern(container_factory_name, user)
+def FactoryGirl.create_generic_file(container_factory_name_or_object, user, file = nil)
+  curation_concern =
+  if container_factory_name_or_object.is_a?(Symbol)
+    FactoryGirl.create_curation_concern(container_factory_name_or_object, user)
+  else
+    container_factory_name_or_object
+  end
+
   generic_file = GenericFile.new
   file ||= Rack::Test::UploadedFile.new(__FILE__, 'text/plain', false)
 
