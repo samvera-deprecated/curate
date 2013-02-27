8f813b5f76bccb124616772bc464f33be8193903
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-02-27 08:54:11 -0500

    Tuning multi-value field labels for accessibility

--- app/inputs/multi_value_input.rb
+++ app/inputs/multi_value_input.rb
@@ -28,11 +28,35 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
     HTML
   end
 
+  # NOTE: There is a one to many relationship between the label and the input
+  # elements. Because of this we can't use the "for" attribute on the label
+  # point to the "id" of the input. Instead we use the "aria-labelledby"
+  # attribute on the input to point to the "id" on the label.
+  #
+  # It would be _better_ to use @builder construct the element but the proper
+  # syntax escapes me.
+  def label
+    <<-HTML
+
+    <label id="#{label_id}" class="string #{label_classes}">#{attribute_name.to_s.titleize}</label>
+    HTML
+  end
+
+  private
+
+  def label_id
+    "#{object_name}_#{attribute_name}_label"
+  end
+
+  def label_classes
+    label_html_options[:class].map{|c| c.to_s}.join(' ')
+  end
+
   def build_text_field(value)
     input_html_options[:value] = value
     input_html_options[:id] = nil
     input_html_options[:class] = "#{object_name}_#{attribute_name}"
-    input_html_options[:'aria-labelledby'] = "#{object_name}_#{attribute_name}_label"
+    input_html_options[:'aria-labelledby'] = label_id
     @builder.text_field(attribute_name, input_html_options)
   end
 
