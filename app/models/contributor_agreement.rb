7bda5cf7c63bbd7b43a11da64961b110e9118270
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-02-28 15:53:46 -0500

    CLA whitespace adjustment

--- app/models/contributor_agreement.rb
+++ app/models/contributor_agreement.rb
@@ -8,15 +8,15 @@ class ContributorAgreement
 
   def human_readable_text
     <<-HTML
-		<article class="human_readable contributor_agreement">
-    <p>
-      Sed mattis, nulla id pulvinar porttitor, nisi erat fermentum sem, ac tempus
-      neque sem ut eros. Aliquam malesuada eros turpis, ut volutpat dolor. Nam vel
-      lorem lacus. Pellentesque lacus metus, aliquam sit amet hendrerit at, laoreet ut
-      lorem. Sed congue interdum faucibus. Nam interdum mi in sem imperdiet placerat.
-      Sed hendrerit volutpat nisi eget ornare.
-    </p>
-		</article>
+    <article class="human_readable contributor_agreement">
+      <p>
+        Sed mattis, nulla id pulvinar porttitor, nisi erat fermentum sem, ac tempus
+        neque sem ut eros. Aliquam malesuada eros turpis, ut volutpat dolor. Nam vel
+        lorem lacus. Pellentesque lacus metus, aliquam sit amet hendrerit at, laoreet ut
+        lorem. Sed congue interdum faucibus. Nam interdum mi in sem imperdiet placerat.
+        Sed hendrerit volutpat nisi eget ornare.
+      </p>
+    </article>
     HTML
   end
 
@@ -24,40 +24,29 @@ class ContributorAgreement
   def legally_binding_text
     <<-HTML
     <article class="lawyer_readable contributor_agreement">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus volutpat
-				nisl ut tincidunt. Cras quis nisl dui, in pharetra odio. Aenean turpis mauris,
-				lobortis sed scelerisque sed, commodo quis mi. Aliquam et odio felis, eget
-				dignissim velit. Quisque eu purus risus, at faucibus nulla. Nulla vitae
-				scelerisque libero. Duis et porttitor ligula. In hac habitasse platea dictumst.
-				Proin porta elementum nibh vel euismod. Nunc aliquam hendrerit ipsum, sed
-				aliquet nisl varius eu. Etiam venenatis tortor in magna lobortis vehicula quis
-				quis est. Maecenas aliquam nisi nec mi luctus quis viverra quam varius. Donec
-				euismod mauris at odio iaculis dapibus.
-      </p>
       <p>
-				Donec vel nunc malesuada enim euismod dictum. Phasellus euismod ornare mattis.
-				Praesent consequat sem id massa iaculis laoreet. Pellentesque ac sagittis risus.
-				Ut neque diam, varius sed adipiscing in, ullamcorper a neque. Etiam porttitor
-				laoreet euismod. Quisque viverra nunc eget eros venenatis vitae scelerisque eros
-				placerat. Mauris placerat vulputate blandit. Morbi sodales orci in nibh posuere
-				euismod. Vivamus dapibus ornare eros eget suscipit. Aliquam erat volutpat. Proin
-				eu scelerisque sem. Pellentesque quis mauris est, quis porta quam. Suspendisse
-				potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-				ridiculus mus.
-      </p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus volutpat
+       nisl ut tincidunt. Cras quis nisl dui, in pharetra odio. Aenean turpis mauris,
+       lobortis sed scelerisque sed, commodo quis mi. Aliquam et odio felis, eget
+       dignissim velit. Quisque eu purus risus, at faucibus nulla. Nulla vitae
+       scelerisque libero. Duis et porttitor ligula. In hac habitasse platea dictumst.
+       Proin porta elementum nibh vel euismod. Nunc aliquam hendrerit ipsum, sed
+       aliquet nisl varius eu. Etiam venenatis tortor in magna lobortis vehicula quis
+       quis est. Maecenas aliquam nisi nec mi luctus quis viverra quam varius. Donec
+       euismod mauris at odio iaculis dapibus.
+     </p>
       <p>
-				Aenean ullamcorper vulputate nibh, sed congue arcu interdum et. Pellentesque
-				habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-				Phasellus in sapien non ipsum tincidunt bibendum vitae eu orci. Nunc hendrerit
-				euismod ipsum, ut egestas leo rutrum eu. Nulla fringilla gravida felis eget
-				vestibulum. Vestibulum pretium, neque eget interdum congue, ante mauris
-				tincidunt mi, eu vehicula neque dui vel urna. Ut tincidunt augue eget lorem
-				ullamcorper pellentesque. Sed blandit velit lectus, ac rutrum leo. In est massa,
-				mattis vitae dictum quis, varius vel tortor. Nullam interdum nulla eget felis
-				laoreet non scelerisque justo varius.
+        Donec vel nunc malesuada enim euismod dictum. Phasellus euismod ornare mattis.
+        Praesent consequat sem id massa iaculis laoreet. Pellentesque ac sagittis risus.
+        Ut neque diam, varius sed adipiscing in, ullamcorper a neque. Etiam porttitor
+        laoreet euismod. Quisque viverra nunc eget eros venenatis vitae scelerisque eros
+        placerat. Mauris placerat vulputate blandit. Morbi sodales orci in nibh posuere
+        euismod. Vivamus dapibus ornare eros eget suscipit. Aliquam erat volutpat. Proin
+        eu scelerisque sem. Pellentesque quis mauris est, quis porta quam. Suspendisse
+        potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus.
       </p>
-		</article>
+    </article>
     HTML
   end
 
