cabb12ab5949d669b9d6ad80152502474f39c005
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-28 13:04:10 -0500

    Adding missing spec for #current_version

--- spec/repository_models/generic_file_spec.rb
+++ spec/repository_models/generic_file_spec.rb
@@ -10,4 +10,12 @@ describe GenericFile do
   it 'uses #noid for #to_param' do
     subject.to_param.should == subject.noid
   end
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:persisted_generic_file) {
+    FactoryGirl.create_generic_file(:senior_thesis, user)
+  }
+  it 'has a current version id' do
+    persisted_generic_file.current_version_id.should == "content.0"
+  end
 end
