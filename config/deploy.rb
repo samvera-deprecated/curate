619ed46a1cf7eed7f6a3881b048702e45dbb7f13
Mark Suhovecky <suhovecky@nd.edu>
2013-02-28 21:46:58 -0500

    Trying set:local_scm_command in deploy.rb for diff git paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,8 @@ ssh_options[:paranoid] = false
 
 set :scm, :git
 set :deploy_via, :remote_cache
-set :scm_command, '/shared/git/bin/git'
+set :scm_command, '/usr/bin/git'
+set :local_scm_command, '/shared/git/bin/git'
 set :branch, "master"
 
 #############################################################
