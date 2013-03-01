a0b3f936f8fd1dcc2c3601be6fb3107e2b2e4e28
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-01 10:57:44 -0500

    Tightening up logic of GenericFile#to_s

--- app/repository_models/generic_file.rb
+++ app/repository_models/generic_file.rb
@@ -4,14 +4,16 @@ class GenericFile
 
   attr_accessor :revised_file, :version
 
+  # Either :revised_file or :file should be the canonical accessor
+  # but for now we have both.
+  alias_attribute :file, :revised_file
+
   def filename
     content.label
   end
 
   def to_s
-    title =  self.title.blank? ? label : self.title
-    title = 'No Title' if title.blank?
-    title
+    title || label || "No Title"
   end
 
   def versions
