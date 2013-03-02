5cc15d3348b0aa3e98efdcd907caf678e674c727
Mark Suhovecky <suhovecky@nd.edu>
2013-03-02 10:40:26 -0500

    Chg Resque Root from ~curatend to ~curatend/current in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -125,9 +125,9 @@ end
 
 namespace :worker do
   task :start, :roles => :work do
-    target_file = "/home/curatend/resque-pool-info"
+    target_file = "$(pwd)/resque-pool-info"
     run [
-      "echo \"RESQUE_POOL_ROOT=$(pwd)\" > #{target_file}",
+      "echo \"RESQUE_POOL_ROOT=$(pwd)/current\" > #{target_file}",
       "echo \"RESQUE_POOL_ENV=#{fetch(:rails_env)}\" >> #{target_file}"
     ].join(" && ")
   end
