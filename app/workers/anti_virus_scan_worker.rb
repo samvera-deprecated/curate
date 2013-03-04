5cdd94748f49975e889ff07c5c5e860a1eb66819
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-04 11:41:06 -0500

    Adding failure test for AntiVirus Scan

--- app/workers/anti_virus_scan_worker.rb
+++ app/workers/anti_virus_scan_worker.rb
@@ -1,9 +1,31 @@
 class AntiVirusScanWorker
+  class AntiVirusScanFailure < RuntimeError
+    def initialize(pid, path)
+      super("Anti Virus scan failed for PID=#{pid.inspect} at PATH=#{path.inspect}")
+    end
+  end
   def queue_name
     :anti_virus
   end
 
-  def initialize(pid)
+  attr_reader :pid, :file_path
+  def initialize(pid, file_path)
     @pid = pid
+    @file_path = file_path
+  end
+
+  def run
+    response_value = anti_virus_instance.call(file_path)
+    if response_value == 0
+      # Spawn a characterization
+    else
+      raise AntiVirusScanFailure.new(pid, file_path)
+      # Failure, sad panda
+    end
+  end
+
+  include Morphine
+  register :anti_virus_instance do
+    ClamAV.instance.method(:scanfile)
   end
 end
--- spec/workers/anti_virus_scan_worker_spec.rb
+++ spec/workers/anti_virus_scan_worker_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 describe AntiVirusScanWorker do
-  subject { AntiVirusScanWorker.new(pid) }
+  subject { AntiVirusScanWorker.new(pid, file.path) }
+  let(:file) { File.new(__FILE__) }
   let(:pid) { 'a1b2c3' }
+  let(:mock_anti_virus_instance) { lambda {|path| anti_virus_scan_status } }
+  let(:anti_virus_scan_status) { nil }
+
   it { subject.queue_name.should == :anti_virus }
+  before(:each) do
+    subject.anti_virus_instance = mock_anti_virus_instance
+  end
+  describe 'successful anti virus scan' do
+    let(:anti_virus_scan_status) { 0 }
+    it 'runs the file upload'
+  end
+  describe 'failed anti virus scan' do
+    let(:anti_virus_scan_status) { 1 }
+    it 'raises an exception' do
+      expect {
+        subject.run
+      }.to raise_error(AntiVirusScanWorker::AntiVirusScanFailure)
+    end
+  end
 end
