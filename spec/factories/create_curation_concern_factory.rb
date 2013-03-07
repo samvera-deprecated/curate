cdaa8a57634eb5c9f25c4f774d7b1317aa99dc08
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-07 10:56:06 -0500

    Update factory to better handle params
    I want to merge the parameters instead of obliterating the underlying
    default

--- spec/factories/create_curation_concern_factory.rb
+++ spec/factories/create_curation_concern_factory.rb
@@ -1,7 +1,7 @@
-def FactoryGirl.create_curation_concern(factory_name, user, attributes = nil)
+def FactoryGirl.create_curation_concern(factory_name, user, override_attributes = nil)
   pid = CurationConcern.mint_a_pid
   curation_concern = factory_name.to_s.classify.constantize.new(pid: pid)
-  attributes ||= FactoryGirl.attributes_for(factory_name)
+  attributes = FactoryGirl.attributes_for(factory_name).reverse_merge(override_attributes)
   actor = CurationConcern.actor(curation_concern, user, attributes)
   actor.create!
   curation_concern
