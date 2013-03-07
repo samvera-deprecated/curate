4b72b20ed508c7d846c7cbd24aac6a861852f188
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-07 11:14:39 -0500

    Updating factory method to fix build

--- spec/factories/create_curation_concern_factory.rb
+++ spec/factories/create_curation_concern_factory.rb
@@ -1,7 +1,7 @@
-def FactoryGirl.create_curation_concern(factory_name, user, override_attributes = nil)
+def FactoryGirl.create_curation_concern(factory_name, user, override_attributes = {})
   pid = CurationConcern.mint_a_pid
   curation_concern = factory_name.to_s.classify.constantize.new(pid: pid)
-  attributes = FactoryGirl.attributes_for(factory_name).reverse_merge(override_attributes)
+  attributes = override_attributes.reverse_merge(FactoryGirl.attributes_for(factory_name))
   actor = CurationConcern.actor(curation_concern, user, attributes)
   actor.create!
   curation_concern
