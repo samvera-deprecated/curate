da0b8ec5e61de1a7396fd2e8e7589af474c4ea26
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-11 17:54:26 -0400

    Adding Content Submissions Terms

--- app/models/contributor_agreement.rb
+++ app/models/contributor_agreement.rb
@@ -25,26 +25,25 @@ class ContributorAgreement
     <<-HTML
     <article class="lawyer_readable contributor_agreement">
       <p>
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus volutpat
-       nisl ut tincidunt. Cras quis nisl dui, in pharetra odio. Aenean turpis mauris,
-       lobortis sed scelerisque sed, commodo quis mi. Aliquam et odio felis, eget
-       dignissim velit. Quisque eu purus risus, at faucibus nulla. Nulla vitae
-       scelerisque libero. Duis et porttitor ligula. In hac habitasse platea dictumst.
-       Proin porta elementum nibh vel euismod. Nunc aliquam hendrerit ipsum, sed
-       aliquet nisl varius eu. Etiam venenatis tortor in magna lobortis vehicula quis
-       quis est. Maecenas aliquam nisi nec mi luctus quis viverra quam varius. Donec
-       euismod mauris at odio iaculis dapibus.
-     </p>
+        I am submitting my work for inclusion in the Curate ND repository maintained by the University Libraries of the University of Notre Dame.
+        I acknowledge that publication of the work may implicate my legal rights with respect to the work and its contents, including my ability to publish the work in other venues.
+        I UNDERSTAND AND AGREE THAT BY SUBMITTING MY CONTENT FOR INCLUSION IN THE CURATEND REPOSITORY, I AGREE TO THE FOLLOWING TERMS:
+      </p>
+
       <p>
-        Donec vel nunc malesuada enim euismod dictum. Phasellus euismod ornare mattis.
-        Praesent consequat sem id massa iaculis laoreet. Pellentesque ac sagittis risus.
-        Ut neque diam, varius sed adipiscing in, ullamcorper a neque. Etiam porttitor
-        laoreet euismod. Quisque viverra nunc eget eros venenatis vitae scelerisque eros
-        placerat. Mauris placerat vulputate blandit. Morbi sodales orci in nibh posuere
-        euismod. Vivamus dapibus ornare eros eget suscipit. Aliquam erat volutpat. Proin
-        eu scelerisque sem. Pellentesque quis mauris est, quis porta quam. Suspendisse
-        potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus.
+        I hereby grant the University of Notre Dame permission to reproduce, edit, publish, disseminate, publicly display, or publicly perform, in whole or in part, the work in any medium at the University&rsquo;s discretion (including but not limited to public websites).
+        I understand that I may be allowed the opportunity to select the intended audience for the materials that I submit, and I agree that I am fully responsible for any claims and all responsibility for materials submitted.
+        The CurateND service is offered as-is with no warranties, express or implied.
+        The University may suspend or terminate CurateND at any time for any reason in the University&rsquo;s sole discretion.
+      </p>
+
+      <p>
+        I warrant that the submitted material is original to me and that I have power to make this agreement.
+        I also warrant that the work has not been previously published elsewhere in whole or in part, and that I do not have any other publication agreements that involve this material or substantial parts of it that conflict with my submission of materials for dissemination in CurateND.
+      </p>
+
+      <p>
+        I understand that I may request the University to remove my submitted materials from the CurateND repository; however, I acknowledge that the University cannot control or retract works that may have been accessed by third parties prior to my request for removal.
       </p>
     </article>
     HTML
