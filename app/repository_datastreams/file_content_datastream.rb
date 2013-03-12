969b245fa9540a992bd153096f50f44e1db24678
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-11 21:36:43 -0400

    Attempting to apply parity of code.
    The FileContentDatastream was implemented in such a way as to assume
    that super was in fact going to work. I would prefer that to not be the
    case. After all, super was working because FileContentDatastream was
    composed of modules that extended/included ActiveSupport::Concern.
    
    What I like about the new FileContentDatastream#run_fits! method is that
    the contents of the method are operating at the same level of
    abstraction.
    
    What I don't like is that it is perhaps more opaque than the traditional
    super method.
    
    I would prefer for the two lines in #run_fits! to have the same
    signatures.

--- app/repository_datastreams/file_content_datastream.rb
+++ app/repository_datastreams/file_content_datastream.rb
@@ -1,17 +1,32 @@
 require Sufia::Engine.root.join('app/models/datastreams/file_content_datastream')
+# I really don't want to touch much of Sufia's underworkings. In doing this
+# I'm able to mimic the #super behavior.
+#
+# More on this method at:
+# http://blog.jayfields.com/2006/12/ruby-alias-method-alternative.html
 class FileContentDatastream
+
+  # Yes, I could be using super, however that assumes a working knowledge
+  # of how the FileContentDatastream is actually crafted (namely via
+  # ActiveSupport::Concern)
+  sufia_run_fits = self.instance_method(:run_fits!)
 
   def run_fits!(file_path)
     anti_virus_scanner(file_path).call
-    if Rails.configuration.respond_to?(:fits_runner)
-      Rails.configuration.fits_runner.call(file_path)
-    else
-      super(file_path)
-    end
+    fits_runner.call(file_path)
   end
 
   protected
   def anti_virus_scanner(file_path)
     AntiVirusScanner.new(self, file_path)
   end
+
+  define_method :fits_runner do
+    if Rails.configuration.respond_to?(:fits_runner)
+      Rails.configuration.fits_runner
+    else
+      sufia_run_fits.bind(self)
+    end
+  end
+
 end
