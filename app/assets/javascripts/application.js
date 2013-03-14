cc0a376ba0dd6f3747837d662516ab78385a8f8e
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-03-14 11:38:08 -0400

    Make sure to use boostrap "improved modal"
    https://github.com/jschr/bootstrap-modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,6 @@
 //
 //= require bootstrap-dropdown
 //= require bootstrap-button
-//= require bootstrap-modal
 //= require bootstrap-collapse
 //= require bootstrap-tooltip
 //= require bootstrap-popover
