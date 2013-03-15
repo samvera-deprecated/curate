55792d76b5c8f70ec9edc4cb943271c1e247bbba
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-03-15 16:24:16 -0400

    Log site release version on help requests

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,18 +1,22 @@
 class HelpRequest < ActiveRecord::Base
 
   attr_accessible(
-    :view_port,
     :current_url,
-    :user_agent,
-    :resolution,
     :flash_version,
+    :how_can_we_help_you,
     :javascript_enabled,
-    :how_can_we_help_you
+    :resolution,
+    :user_agent,
+    :view_port
   )
+
+  attr_reader :site_version
 
   belongs_to :user
   validates_presence_of :how_can_we_help_you,
     :message => "Please tell us about the problem or issue you are having with CurateND."
+
+  after_initialize :set_release_version
 
   def browser_name
     parse_user_agent
@@ -28,5 +32,9 @@ class HelpRequest < ActiveRecord::Base
 
   def parse_user_agent
     @browser ||= Browser.new(:ua => user_agent)
+  end
+
+  def set_release_version
+    self.release_version ||= Rails.configuration.build_identifier
   end
 end
