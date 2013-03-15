121d437135726321ccdd9c882539ab592e7a1e75
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-15 15:04:13 -0400

    Updating User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ActiveRecord::Base
-# Connects this user object to Sufia behaviors.
- include Sufia::User
-# Connects this user object to Sufia behaviors.
- include Sufia::User
-# Connects this user object to Hydra behaviors.
- include Hydra::User
-# Connects this user object to Blacklights Bookmarks.
- include Blacklight::User
+  # Connects this user object to Hydra behaviors.
+  include Hydra::User
+  # Connects this user object to Blacklights Bookmarks.
+  include Blacklight::User
+  # Connects this user object to Sufia behaviors.
+  include Sufia::User
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :cas_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :remember_me, :username#, :password
