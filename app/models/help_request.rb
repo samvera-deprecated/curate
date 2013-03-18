6a458806e0e999157cf42342970db48bd3eef860
Dan Brubaker Horst <dbrubak1@nd.edu>
2013-03-18 13:49:21 -0400

    Protecting application version; form styling
    I suspect that there is a better way to do this.

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -10,13 +10,15 @@ class HelpRequest < ActiveRecord::Base
     :view_port
   )
 
+  attr_reader :application_version
   attr_reader :release_version
 
   belongs_to :user
   validates_presence_of :how_can_we_help_you,
     :message => "Please tell us about the problem or issue you are having with CurateND."
 
-  after_initialize :set_release_version
+  after_initialize :set_application_version
+  before_create :set_release_version
 
   def browser_name
     parse_user_agent
@@ -34,7 +36,17 @@ class HelpRequest < ActiveRecord::Base
     @browser ||= Browser.new(:ua => user_agent)
   end
 
+  # We don't want the release version to be set directly by the user.
+  # However, we need to display the application verson _before_ the record is saved.
+  def set_application_version
+    if self.release_version
+      @application_version = self.release_version
+    else
+      @application_version = Rails.configuration.build_identifier
+    end
+  end
+
   def set_release_version
-    self.release_version ||= Rails.configuration.build_identifier
+    self.release_version = self.application_version
   end
 end
