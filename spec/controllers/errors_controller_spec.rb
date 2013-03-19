be341da13c65664fdb0e93649deabc997d7660b4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-18 23:33:15 -0400

    Attempting to catch exceptions that bubble up
    http://agileleague.com/2012/12/rails-3-2-custom-error-pages-the-exceptions_app-and-testing-with-capybara/

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,20 +1,48 @@
 require 'spec_helper'
 
 describe ErrorsController do
-  it 'GET #not_found' do
-    get :not_found
-    expect(response).to render_template('not_found')
+  describe '#not_found' do
+    it 'GET' do
+      get :not_found
+      expect(response).to render_template('not_found')
+      expect(response.status).to eq(404)
+    end
+    it 'PUT' do
+      put :not_found
+      expect(response).to render_template('not_found')
+      expect(response.status).to eq(404)
+    end
+    it 'DELETE' do
+      delete :not_found
+      expect(response).to render_template('not_found')
+      expect(response.status).to eq(404)
+    end
+    it 'POST' do
+      post :not_found
+      expect(response).to render_template('not_found')
+      expect(response.status).to eq(404)
+    end
   end
-  it 'PUT #not_found' do
-    put :not_found
-    expect(response).to render_template('not_found')
-  end
-  it 'DELETE #not_found' do
-    delete :not_found
-    expect(response).to render_template('not_found')
-  end
-  it 'POST #not_found' do
-    post :not_found
-    expect(response).to render_template('not_found')
+  describe '#unauthorized' do
+    it 'GET' do
+      get :unauthorized
+      expect(response).to render_template('unauthorized')
+      expect(response.status).to eq(401)
+    end
+    it 'PUT' do
+      put :unauthorized
+      expect(response).to render_template('unauthorized')
+      expect(response.status).to eq(401)
+    end
+    it 'DELETE' do
+      delete :unauthorized
+      expect(response).to render_template('unauthorized')
+      expect(response.status).to eq(401)
+    end
+    it 'POST' do
+      post :unauthorized
+      expect(response).to render_template('unauthorized')
+      expect(response.status).to eq(401)
+    end
   end
 end
