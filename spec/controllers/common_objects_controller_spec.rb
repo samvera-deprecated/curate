ad6308b6e5ddce6fe5cb063280d7926c36030e47
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-19 14:08:48 -0400

    Adding behavior for common objects display

--- spec/controllers/common_objects_controller_spec.rb
+++ spec/controllers/common_objects_controller_spec.rb
@@ -3,15 +3,14 @@ require 'spec_helper'
 describe CommonObjectsController do
   render_views
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:another_user) { FactoryGirl.create(:user) }
+  let(:visibility) { nil }
+  let(:curation_concern) {
+    FactoryGirl.create_curation_concern(:senior_thesis, user, visibility: visibility)
+  }
   describe '#show' do
-    let(:user) { FactoryGirl.create(:user) }
     let(:template_for_success) { 'show' }
-    let(:template_for_unauthorized ) { '/errors/unauthorized' }
-    let(:another_user) { FactoryGirl.create(:user) }
-    let(:visibility) { nil }
-    let(:curation_concern) {
-      FactoryGirl.create_curation_concern(:senior_thesis, user, visibility: visibility)
-    }
     describe '"Open Access" object' do
       let(:visibility) { 'open' }
       it 'renders for unauthenticated person' do
@@ -37,10 +36,10 @@ describe CommonObjectsController do
 
     describe '"Restricted" object' do
       let(:visibility) { 'restricted' }
-      it 'does not display for unauthenticated person' do
+      it 'redirect to DOI information for unauthenticated person' do
         get :show, id: curation_concern.to_param
-        response.status.should == 401
-        expect(response).to render_template(template_for_unauthorized)
+        response.status.should == 302
+        expect(response).to redirect_to(common_object_stub_information_path(curation_concern))
       end
 
       it 'renders for the creator' do
@@ -53,16 +52,16 @@ describe CommonObjectsController do
       it 'renders for the creator' do
         sign_in(another_user)
         get :show, id: curation_concern.to_param
-        response.status.should == 401
-        expect(response).to render_template(template_for_unauthorized)
+        response.status.should == 302
+        expect(response).to redirect_to(common_object_stub_information_path(curation_concern))
       end
     end
     describe '"Institution Only" object' do
       let(:visibility) { 'psu' }
       it 'does not display for unauthenticated person' do
         get :show, id: curation_concern.to_param
-        response.status.should == 401
-        expect(response).to render_template(template_for_unauthorized)
+        response.status.should == 302
+        expect(response).to redirect_to(common_object_stub_information_path(curation_concern))
       end
 
       it 'renders for the creator' do
@@ -81,4 +80,31 @@ describe CommonObjectsController do
     end
   end
 
+  describe '#show_stub_information' do
+    let(:template_for_success) { 'show_stub_information' }
+    describe '"Open Access" object' do
+      let(:visibility) { 'open' }
+      it 'renders rudimentary DOI information' do
+        get :show_stub_information, id: curation_concern.to_param
+        response.status.should == 200
+        expect(response).to render_template(template_for_success)
+      end
+    end
+    describe '"Restricted" object' do
+      let(:visibility) { 'restricted' }
+      it 'renders rudimentary DOI information' do
+        get :show_stub_information, id: curation_concern.to_param
+        response.status.should == 200
+        expect(response).to render_template(template_for_success)
+      end
+    end
+    describe '"Institution Only" object' do
+      let(:visibility) { 'psu' }
+      it 'renders rudimentary DOI information' do
+        get :show_stub_information, id: curation_concern.to_param
+        response.status.should == 200
+        expect(response).to render_template(template_for_success)
+      end
+    end
+  end
 end
