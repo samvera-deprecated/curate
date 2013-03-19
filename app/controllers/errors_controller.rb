d50692f031f0f62dc28229de2fd61515f8cffd87
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-18 23:33:15 -0400

    Attempting to catch exceptions that bubble up
    http://agileleague.com/2012/12/rails-3-2-custom-error-pages-the-exceptions_app-and-testing-with-capybara/

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,4 +2,8 @@ class ErrorsController < ApplicationController
   def not_found
     render 'not_found', status: :not_found, layout: 'curate_nd'
   end
+
+  def unauthorized
+    render("unauthorized", status: :unauthorized, layout: 'curate_nd')
+  end
 end
