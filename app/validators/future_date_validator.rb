8dcda6e69f3134501ade7e3e764b173fbacb722d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-22 14:14:37 -0400

    Refactoring future date validator

--- app/validators/future_date_validator.rb
+++ app/validators/future_date_validator.rb
@@ -3,13 +3,16 @@ class FutureDateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
       begin
-        Date.parse(value)
-        record.errors[:embargo_release_date] << "Must be a future date" unless Date.parse(value)> Date.today
-      rescue ArgumentError => e
+        if date = value.to_date
+          if date <= Date.today
+            record.errors[:embargo_release_date] << "Must be a future date"
+          end
+        else
+          record.errors[:embargo_release_date] << "Invalid Date Format"
+        end
+      rescue NoMethodError
         record.errors[:embargo_release_date] << "Invalid Date Format"
-        return
       end
-
     end
   end
 
