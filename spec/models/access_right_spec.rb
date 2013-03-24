5d05f631098f6e857520de38ecefc82602cb90ab
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-03-24 11:43:51 -0400

    Refactoring access rights tests for bombardment

--- spec/models/access_right_spec.rb
+++ spec/models/access_right_spec.rb
@@ -1,47 +1,41 @@
 require 'spec_helper'
 
 describe AccessRight do
-  let(:permissionable) {
-    double('permissionable', permissions: permissions, visibility: visibility)
-  }
-  let(:permissions) { [{access: :edit, name: permission_name}] }
-  let(:permission_name) { nil }
-  let(:visibility) { nil }
-  subject { AccessRight.new(permissionable) }
+  [
+    [false, AccessRight::PERMISSION_TEXT_VALUE_PUBLIC, nil, true, false, false],
+    [false, AccessRight::PERMISSION_TEXT_VALUE_AUTHENTICATED, nil, false, true, false],
+    [false, nil, nil, false, false, true],
+    [false, nil, AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC, true, false, false],
+    [false, nil, AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED, false, true, false],
+    [false, nil, AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE, false, false, true],
+  ].each do |given_persisted, givin_permission, given_visibility, expected_open_access, expected_authentication_only, expected_private|
+    spec_text = <<-TEXT
 
-  describe 'with #visibility not set' do
-    let(:visibility) { nil }
-    describe 'open_access?' do
-      let(:permission_name) { AccessRight::PERMISSION_TEXT_VALUE_PUBLIC }
-      it { expect(subject).to be_open_access }
-    end
+    EXPECTED: {
+      open_access: #{expected_open_access},
+      restricted: #{expected_authentication_only},
+      private: #{expected_private}
+    },
+    GIVEN: {
+      persisted: #{given_persisted},
+      permission: #{givin_permission},
+      visibility: #{given_visibility}
+    }
+    TEXT
 
-    describe 'authenticated_only?' do
-      let(:permission_name) { AccessRight::PERMISSION_TEXT_VALUE_AUTHENTICATED }
-      it { expect(subject).to be_authenticated_only }
-    end
+    it spec_text do
+      permissions = [{access: :edit, name: givin_permission}]
+      permissionable = double(
+        'permissionable',
+        permissions: permissions,
+        visibility: given_visibility,
+        persisted?: given_persisted
+      )
+      access_right = AccessRight.new(permissionable)
 
-    describe 'private?' do
-      let(:permission_name) { nil }
-      it { expect(subject).to be_private }
-    end
-  end
-
-  describe 'with #visibility set' do
-    let(:permission_name) { nil }
-    describe 'open_access?' do
-      let(:visibility) { AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
-      it { expect(subject).to be_open_access }
-    end
-
-    describe 'authenticated_only?' do
-      let(:visibility) { AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED }
-      it { expect(subject).to be_authenticated_only }
-    end
-
-    describe 'private?' do
-      let(:visibility) { AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE }
-      it { expect(subject).to be_private }
+      expect(access_right.open_access?).to eq(expected_open_access)
+      expect(access_right.authenticated_only?).to eq(expected_authentication_only)
+      expect(access_right.private?).to eq(expected_private)
     end
   end
 end
