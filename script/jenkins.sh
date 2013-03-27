4493c7a3a8d45570f86408cd2233004f27895d95
rbalekai <rbalekai@nd.edu>
2013-03-27 09:12:43 -0400

    Add script to clear solr for ci environment.

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -14,6 +14,7 @@ export PATH=/shared/git/bin:$PATH
 export PATH=/global/soft/fits/current:/shared/fedora_prod36/java/bin:$PATH
 export RAILS_ENV=ci
 export HEADLESS=true
+RUBY=/shared/ruby_prod/ruby/1.9.3/bin/ruby
 
 echo "=-=-=-=-=-=-=-= bundle install"
 /shared/ruby_prod/ruby/1.9.3/bin/bundle install --path="$WORKSPACE/vendor/bundle" --binstubs="$WORKSPACE/vendor/bundle/bin" --shebang '/shared/ruby_prod/ruby/1.9.3/bin/ruby' --deployment --gemfile="$WORKSPACE/Gemfile"
@@ -44,9 +45,19 @@ done
 echo "=-=-=-=-=-=-=-= copy secret_token.rb"
 cp secret_ci/curate_nd/secret_token.rb config/initializers/secret_token.rb
 
+echo "=-=-=-=-=-=-=-= Clearing Solr Documents"
+RUBY_SCRIPT="data = YAML.load(File.read('config/solr.yml')); puts data['ci']['url'];"
+SOLR_URL=$($RUBY -ryaml -e "$RUBY_SCRIPT")
+curl "$SOLR_URL/update?commit=true" -H 'Content-Type:application/xml' -d '<delete><query>*:*</query></delete>'
+
+echo "=-=-=-=-=-=-=-= Clearing Fedora Objects and database indexes."
+# Code to clear fedora goes here.
+echo "=-=-=-=-=-=-=-= Finished cleaning"
+
 echo "=-=-=-=-=-=-=-= rake curatend:ci"
 $WORKSPACE/vendor/bundle/bin/rake --trace curatend:ci
 retval=$?
 
 echo "=-=-=-=-=-=-=-= $0 finished $retval"
+
 exit $retval
