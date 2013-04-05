d1d5dd782c9742582158696dc735a632a9ac4d46
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-04 21:51:17 -0400

    Tweaking CI rake task for travis

--- spec/factories/senior_thesis_factory.rb
+++ spec/factories/senior_thesis_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
-  factory :senior_thesis, class: SeniorThesis do
+  factory :senior_thesis, class: 'SeniorThesis' do
     sequence(:title) {|n| "Title #{n}"}
     rights { Sufia::Engine.config.cc_licenses.keys.first }
   end
-  factory :senior_thesis_invalid, class: SeniorThesis do
+  factory :senior_thesis_invalid, class: 'SeniorThesis' do
     title ''
   end
 end
