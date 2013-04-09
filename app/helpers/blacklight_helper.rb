9a6e5dedaa340f664b014404395a8e9bcababeb5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-09 11:40:55 -0400

    Extracting helpers to curate

--- app/helpers/blacklight_helper.rb
+++
@@ -1,7 +0,0 @@
-module BlacklightHelper
-  include Blacklight::BlacklightHelperBehavior
-
-  def application_name
-    "CurateND"
-  end
-end
--- app/helpers/common_objects_helper.rb
+++
@@ -1,5 +0,0 @@
-module CommonObjectsHelper
-  def common_object_partial_for(object)
-    object.to_partial_path.sub(/\A.*\/([^\/]*)\Z/,'common_objects/\1')
-  end
-end
