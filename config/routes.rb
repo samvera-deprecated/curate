8e15ae2d3105d1cc4ed8afd362650f060a29c5fc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-09 13:14:15 -0400

    Adding routes from curate_nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  mount_roboto
+
+  Blacklight.add_routes(self)
+
+  devise_for :users
+
+  resources 'dashboard', :only=>:index do
+    collection do
+      get 'page/:page', :action => :index
+      get 'facet/:id',  :action => :facet, :as => :dashboard_facet
+      get 'related/:id',:action => :get_related_file, :as => :related_file
+    end
+  end
+  resources :downloads, only: [:show]
+
   namespace :curation_concern, path: :concern do
     resources(
       :generic_files,
@@ -10,4 +25,13 @@ Rails.application.routes.draw do
       only: [:show, :edit, :update, :destroy]
     )
   end
+
+  resources :terms_of_service_agreements, only: [:new, :create]
+  resources :help_requests, only: [:new, :create]
+  resources :classify_concerns, only: [:new, :create]
+
+  match "show/:id" => "common_objects#show", via: :get, as: "common_object"
+  match "show/stub/:id" => "common_objects#show_stub_information", via: :get, as: "common_object_stub_information"
+  root to: 'welcome#index'
+
 end
