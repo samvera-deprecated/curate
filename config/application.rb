6e6e6b4064dde80d1133581e840f76e20c152308
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-11 12:31:53 -0400

    Removing explicit sufia require
    I'd rather the curate gem negotiate that

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ end
 module CurateNd
   class Application < Rails::Application
     require 'curate'
-    require 'sufia'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
