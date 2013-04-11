0779f50ca22cd3b026f889449872bb788305fedb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-11 12:29:59 -0400

    Regrouping methods to reflect relationships

--- app/controllers/common_objects_controller.rb
+++ app/controllers/common_objects_controller.rb
@@ -1,19 +1,18 @@
 require File.expand_path('../../helpers/common_objects_helper', __FILE__)
 class CommonObjectsController < ApplicationController
+  layout 'common_objects'
+
   respond_to(:html)
   include Sufia::Noid # for normalize_identifier method
-
+  prepend_before_filter :normalize_identifier
   def curation_concern
     @curation_concern ||= ActiveFedora::Base.find(params[:id], cast: true)
   end
   before_filter :curation_concern
-  before_filter :enforce_show_permissions, only: [:show]
   helper_method :curation_concern
-  prepend_before_filter :normalize_identifier
-  layout 'common_objects'
-
   helper CommonObjectsHelper
 
+  before_filter :enforce_show_permissions, only: [:show]
   rescue_from Hydra::AccessDenied do |exception|
     redirect_to common_object_stub_information_path(curation_concern)
   end
