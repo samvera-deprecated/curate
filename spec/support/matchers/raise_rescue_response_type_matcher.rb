90178a3da21ee7626e62f687251bde7b13fe28e7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-11 15:48:46 -0400

    Improving messages for raise_rescue_response_type

--- spec/support/matchers/raise_rescue_response_type_matcher.rb
+++ spec/support/matchers/raise_rescue_response_type_matcher.rb
@@ -12,6 +12,7 @@ RSpec::Matchers.define :raise_rescue_response_type do |expected_rescue_response|
     end
 
     if @exception.nil?
+      @actual_rescue_response = @status
       @status == Rack::Utils.status_code(@expected_rescue_response)
     else
       response = ActionDispatch::ExceptionWrapper.rescue_responses[@exception.class.name]
@@ -25,11 +26,17 @@ RSpec::Matchers.define :raise_rescue_response_type do |expected_rescue_response|
   end
 
   failure_message_for_should do |text|
-    "expected to raise an exception with rescue_response #{@expected_rescue_response.inspect} instead got #{@actual_rescue_response.inspect}"
+    text = "expected to raise an exception with rescue_response"
+    text << " #{@expected_rescue_response.inspect} instead got #{@actual_rescue_response.inspect}"
+    text << " (Exception #{@exception.class}: #{@exception})" if @exception
+    text
   end
 
   failure_message_for_should_not do |text|
-    "expected to NOT raise an exception with rescue_response #{@expected_rescue_response.inspect} but got #{@actual_rescue_response.inspect}"
+    text = "expected to NOT raise an exception with rescue_response"
+    text << " #{@expected_rescue_response.inspect} but got #{@actual_rescue_response.inspect}"
+    text << " (Exception #{@exception.class}: #{@exception})" if @exception
+    text
   end
 
 end
