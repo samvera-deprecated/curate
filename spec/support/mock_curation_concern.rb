a6c3e1351f8290fb19340e7e526440d8b3ba1715
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-12 14:30:36 -0400

    Tidying up code

--- spec/support/mock_curation_concern.rb
+++ spec/support/mock_curation_concern.rb
@@ -1,23 +1,24 @@
 class MockCurationConcern < ActiveFedora::Base
   class MetadataDatastream < ActiveFedora::NtriplesRDFDatastream
-  map_predicates do |map|
-    map.title(in: RDF::DC) do |index|
-      index.as :searchable, :displayable
+    map_predicates do |map|
+      map.title(in: RDF::DC) do |index|
+        index.as :searchable, :displayable
+      end
+      map.created(in: RDF::DC)
+      map.creator(in: RDF::DC) do |index|
+        index.as :searchable, :facetable, :displayable
+      end
+      map.date_uploaded(to: "dateSubmitted", in: RDF::DC) do |index|
+        index.type :date
+        index.as :searchable, :displayable, :sortable
+      end
+      map.date_modified(to: "modified", in: RDF::DC) do |index|
+        index.type :date
+        index.as :searchable, :displayable, :sortable
+      end
+      map.part(:to => "hasPart", in: RDF::DC)
+      map.identifier({in: RDF::DC})
     end
-    map.created(in: RDF::DC)
-    map.creator(in: RDF::DC) do |index|
-      index.as :searchable, :facetable, :displayable
-    end
-    map.date_uploaded(to: "dateSubmitted", in: RDF::DC) do |index|
-      index.type :date
-      index.as :searchable, :displayable, :sortable
-    end
-    map.date_modified(to: "modified", in: RDF::DC) do |index|
-      index.type :date
-      index.as :searchable, :displayable, :sortable
-    end
-    map.part(:to => "hasPart", in: RDF::DC)
-    map.identifier({in: RDF::DC})
   end
 end
   include Hydra::ModelMixins::CommonMetadata
