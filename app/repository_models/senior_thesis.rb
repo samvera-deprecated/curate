c6109712564f7a8331f49672b12b91cbb21e3189
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-15 10:56:16 -0400

    Updating SeniorThesis to use modules

--- app/repository_models/senior_thesis.rb
+++ app/repository_models/senior_thesis.rb
@@ -1,22 +1,13 @@
 #require 'datastreams/properties_datastream'
 #require_relative './generic_file'
+require 'curation_concern/model'
 class SeniorThesis < ActiveFedora::Base
-  include Hydra::ModelMixins::CommonMetadata
-  include Sufia::ModelMethods
-  include Sufia::Noid
-  include Sufia::GenericFile::Permissions
+  include CurationConcern::Model
+  include CurationConcern::WithGenericFiles
   include CurationConcern::Embargoable
   include CurationConcern::WithAccessRight
 
-  has_metadata name: "properties", type: PropertiesDatastream, control_group: 'M'
   has_metadata name: "descMetadata", type: SeniorThesisMetadataDatastream, control_group: 'M'
-
-  has_many :generic_files, property: :is_part_of
-
-  after_destroy :after_destroy_cleanup
-  def after_destroy_cleanup
-    generic_files.each(&:destroy)
-  end
 
   delegate_to(
     :descMetadata,
@@ -48,32 +39,11 @@ class SeniorThesis < ActiveFedora::Base
       :subject
     ]
   )
-  delegate_to :properties, [:relative_path, :depositor], unique: true
 
   validates :title, presence: { message: 'Your thesis must have a title.' }
   validates :rights, presence: { message: 'You must select a license for your work.' }
 
-  before_save {|obj| obj.archived_object_type = self.human_readable_type }
-
-  def human_readable_type
-    self.class.to_s.demodulize.titleize
-  end
-
-  def to_solr(solr_doc={}, opts={})
-    super(solr_doc, opts)
-    solr_doc["noid_s"] = noid
-    return solr_doc
-  end
-
   attr_accessor :thesis_file, :assign_doi
-
-  def to_param
-    noid
-  end
-
-  def to_s
-    title
-  end
 
   def doi_url
     File.join(Rails.configuration.doi_url, self.identifier)
