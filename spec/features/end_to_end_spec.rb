5674a6adf172bcd1f0124dcc3371b5c12521e27d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-19 10:58:47 -0400

    Adjusting tests to reflect user signin counts

--- spec/features/end_to_end_spec.rb
+++ spec/features/end_to_end_spec.rb
@@ -18,7 +18,14 @@ describe 'end to end behavior', describe_options do
     Warden.test_reset!
     Resque.inline = @old_resque_inline_value
   end
-  let(:user) { FactoryGirl.create(:user, agreed_to_terms_of_service: agreed_to_terms_of_service) }
+  let(:sign_in_count) { 0 }
+  let(:user) {
+    FactoryGirl.create(
+      :user,
+      agreed_to_terms_of_service: agreed_to_terms_of_service,
+      sign_in_count: sign_in_count
+    )
+  }
   let(:another_user) { FactoryGirl.create(:user, agreed_to_terms_of_service: true) }
   let(:prefix) { Time.now.strftime("%Y-%m-%d-%H-%M-%S-%L") }
   let(:initial_title) { "#{prefix} Something Special" }
@@ -117,6 +124,7 @@ describe 'end to end behavior', describe_options do
 
   describe 'help request' do
     let(:agreed_to_terms_of_service) { true }
+    let(:sign_in_count) { 20 }
     # I want to test both JS mode and non-JS mode
     [true, false].each do |using_javascript|
       it "is available for users who are authenticated and agreed to ToS", js: using_javascript do
@@ -213,6 +221,7 @@ describe 'end to end behavior', describe_options do
 
   describe 'with a user who has not agreed to terms of service' do
     let(:agreed_to_terms_of_service) { false }
+    let(:sign_in_count) { 1 }
     it "displays the terms of service page after authentication" do
       login_as(user)
       get_started
