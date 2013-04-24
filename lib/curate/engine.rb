aa5e81ddd7523aad3f6861542208585177c1b9a8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-24 14:49:53 -0400

    Eliminating double Dashboard breadcrumb rendering
    This is an attempt to remove the double Dashboard breadcrumb rendering
    problem.
    
    @WIP

--- lib/curate/engine.rb
+++ lib/curate/engine.rb
@@ -17,6 +17,7 @@ module Curate
     end
 
     autoload :CatalogController, File.expand_path('../../../app/controllers/catalog_controller', __FILE__)
+    autoload :ApplicationController, File.expand_path('../../../app/controllers/application_controller', __FILE__)
 
     config.action_dispatch.rescue_responses["ActionController::RoutingError"] = :not_found
     config.action_dispatch.rescue_responses["ActiveFedora::ObjectNotFoundError"] = :not_found
@@ -26,10 +27,10 @@ module Curate
     config.action_dispatch.rescue_responses["Rubydora::RecordNotFound"] = :not_found
 
     initializer "curate" do |app|
+
       require File.expand_path('../../../app/repository_models/generic_file', __FILE__)
       require File.expand_path('../../../app/models/solr_document', __FILE__)
       require File.expand_path('../../../app/builders/accordion_builder_helper', __FILE__)
-      # require File.expand_path('../../../app/workers/characterize_job', __FILE__)
 
       module WithAntiVirusHandler
         def run
@@ -41,11 +42,9 @@ module Curate
       end
       CharacterizeJob.send(:include, WithAntiVirusHandler)
 
-      require File.expand_path('../../../app/controllers/application_controller', __FILE__)
       require File.expand_path('../../../app/controllers/downloads_controller', __FILE__)
       require File.expand_path('../../../app/controllers/errors_controller', __FILE__)
       require File.expand_path('../../../app/builders/bootstrap_breadcrumbs_builder', __FILE__)
-      require File.expand_path('../../../app/controllers/dashboard_controller', __FILE__)
       require File.expand_path('../../../app/services/curation_concern', __FILE__)
 
       require File.expand_path('../../../app/helpers/blacklight_helper', __FILE__)
