bde2733fabf07ff3bbf6d40ad3893750c46c54b1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-24 15:22:55 -0400

    Updating ClassifyConcern constant name

--- app/models/classify_concern.rb
+++ app/models/classify_concern.rb
@@ -1,6 +1,6 @@
 require Curate::Engine.root.join('app/models/classify_concern')
 class ClassifyConcern
-  VALID_CURATION_CONCERNS = {
-    'Senior Thesis' => 'SeniorThesis'
-  }
+  VALID_CURATION_CONCERN_CLASS_NAMES = [
+    'SeniorThesis'
+  ]
 end
