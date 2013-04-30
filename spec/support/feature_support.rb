10695aab5342e4fa0578cae912891224febe3cf2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-04-30 11:49:34 -0400

    Removing .load_and_authorize_resource
    The CanCan.load_and_authorize_resource was misbehaving in integration
    tests. I suspect this could've become a problem in production as well.
    So I have since replaced the .load_and_authorize_resource with the
    configurable:
      Controller.excluded_actions_for_curation_concern_authorization

--- spec/support/feature_support.rb
+++ spec/support/feature_support.rb
@@ -3,4 +3,7 @@ module FeatureSupport
     user.reload # because the user isn't re-queried via Warden
     super(user, scope: :user, run_callbacks: false)
   end
+  def logout(user=:user)
+    super(user)
+  end
 end
