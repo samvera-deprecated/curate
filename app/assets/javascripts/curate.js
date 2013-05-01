acb080e87be9cec0e8c3ee288bc3bbaecab72aad
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-05-01 14:54:13 -0400

    Removing auto-collapse feature for accordion
    This was causing problems in browsers. So until it is fixed, it is out.

--- app/assets/javascripts/curate.js
+++ app/assets/javascripts/curate.js
@@ -46,11 +46,11 @@ $(function(){
   $('.multi_value.control-group').manage_fields();
 
   // Collapse all of the accordion body elements except the first
-  $('.accordion-body').each(function(index){
-    if(index != 0) {
-      $(this).removeClass('in');
-    }
-  });
+  // $('.accordion-body').each(function(index){
+  //   if(index != 0) {
+  //     $(this).removeClass('in');
+  //   }
+  // });
 
   $('.datepicker').datepicker({
     format: 'yyyy-mm-dd' });
