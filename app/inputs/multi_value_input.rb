1ff4da1a90a857caac47dc9f61b924c17057e01b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-05-08 14:27:25 -0400

    Updating multi-value input for label rendering
    Unfortunately the #label implementation was a bit naive, so I'm removing
    that implementation and leaning on the existing implementation.

--- app/inputs/multi_value_input.rb
+++ app/inputs/multi_value_input.rb
@@ -1,6 +1,8 @@
 require 'simple_form'
 class MultiValueInput < SimpleForm::Inputs::CollectionInput
   def input
+    prepare_input!
+    @rendered_first_element = false
     input_html_classes.unshift("string")
     input_html_options[:type] ||= 'text'
     input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
@@ -10,11 +12,11 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
         <ul class="listing">
     HTML
 
-    collection.each do |value|
+    collection.each_with_index do |value, i|
       unless value.to_s.strip.blank?
         markup << <<-HTML
           <li class="field-wrapper">
-            #{build_text_field(value, '')}
+            #{build_text_field(value)}
           </li>
         HTML
       end
@@ -22,54 +24,46 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
 
     markup << <<-HTML
           <li class="field-wrapper">
-            #{build_text_field('', 'add')}
+            #{build_text_field('')}
           </li>
         </ul>
 
     HTML
   end
 
-  # NOTE: There is a one to many relationship between the label and the input
-  # elements. Because of this we can't use the "for" attribute on the label
-  # point to the "id" of the input. Instead we use the "aria-labelledby"
-  # attribute on the input to point to the "id" on the label.
-  #
-  # It would be _better_ to use @builder construct the element but the proper
-  # syntax escapes me.
-  def label
-    <<-HTML
-    <label id="#{label_id}" class="string #{label_classes}">#{label_text_for(attribute_name)}</label>
-    HTML
-  end
-
   private
 
-  def label_text_for(attribute_name)
-    return options[:label] if options[:label]
-    template.get_label(attribute_name)
+
+  def build_text_field(value)
+    options = input_html_options.dup
+
+    options[:value] = value
+    if @rendered_first_element
+      options[:id] = nil
+    else
+      options[:id] ||= input_dom_id
+    end
+    options[:class] ||= ''
+    options[:class] << " #{input_dom_id} multi-text-field"
+    options[:'aria-labelledby'] = label_id
+    @rendered_first_element = true
+    @builder.text_field(attribute_name, options)
   end
+
 
   def label_id
-    "#{object_name}_#{attribute_name}_label"
+    input_dom_id + '_label'
   end
 
-  def label_classes
-    label_html_options[:class].map{|c| c.to_s}.join(' ')
+  def input_dom_id
+    input_html_options[:id] || "#{object_name}_#{attribute_name}"
   end
 
-  def build_text_field(value, append_to_id)
-    input_html_options[:value] = value
-    input_html_options[:id] = append_to_id.blank? ? "#{attribute_name}" : "#{attribute_name}_#{append_to_id}"
-    input_html_options[:class] = "#{object_name}_#{attribute_name} multi-text-field"
-    input_html_options[:'aria-labelledby'] = label_id
-    @builder.text_field(attribute_name, input_html_options)
-  end
-
-  protected
   def collection
     @collection ||= begin
       object.send(attribute_name)
     end
   end
+
   def multiple?; true; end
 end
