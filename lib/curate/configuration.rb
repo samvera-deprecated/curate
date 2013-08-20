c7b40076a36f724396ccbcde6f9d3fbfb10b79fc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-20 11:11:28 -0400

    Adding Curate.configuration.build_identifier
    Replaces Rails.config.build_identifier

--- lib/curate/configuration.rb
+++ lib/curate/configuration.rb
@@ -13,10 +13,17 @@ module Curate
     # and returns AntiVirusScanner::NO_VIRUS_FOUND_RETURN_VALUE if no
     # virus is found; Any other returned value means a virus was found
     attr_accessor :default_antivirus_instance
+
+    # When was this last built/deployed
+    attr_accessor :build_identifier
     def initialize
-      @default_antivirus_instance =     lambda {|file_path|
+      @default_antivirus_instance = lambda {|file_path|
         AntiVirusScanner::NO_VIRUS_FOUND_RETURN_VALUE
       }
+      # If you restart the server, this could be out of sync; A better
+      # implementation is to read something from the file system. However
+      # that detail is an exercise for the developer.
+      @build_identifier = Time.now.strftime("%Y-%m-%d %H:%M:%S")
     end
   end
 
