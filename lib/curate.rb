454ba190aaed5e85d905b12a859dc68a903bf9de
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-20 13:14:43 -0400

    Adding Curate::User mixin

--- lib/curate.rb
+++ lib/curate.rb
@@ -1,6 +1,7 @@
 require 'sufia/models'
 require "curate/engine"
 require "curate/configuration"
+require "curate/user"
 require 'simple_form'
 require 'bootstrap-datepicker-rails'
 require 'hydra-batch-edit'
