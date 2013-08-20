cacd6bb10c99652d7e5aa152bf072dd0b3994b86
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-20 10:42:54 -0400

    Reseting AV configuration in spec
    Configuration tests should reset the default configuration once they
    are completed.

--- spec/lib/curate/configuration_spec.rb
+++ spec/lib/curate/configuration_spec.rb
@@ -2,19 +2,23 @@ require 'spec_helper'
 module Curate
   describe Configuration do
     describe '#default_antivirus_instance' do
+      before(:each) do
+        @previous_av_instance = Curate.configuration.default_antivirus_instance
+      end
+      after(:each) do
+        Curate.configuration.default_antivirus_instance = @previous_av_instance
+      end
       it 'has a default callable' do
         expect(Curate.configuration.default_antivirus_instance.call(__FILE__)).
           to eq(AntiVirusScanner::NO_VIRUS_FOUND_RETURN_VALUE)
       end
 
       it 'is configurable' do
-        previous_av_instance = Curate.configuration.default_antivirus_instance
         Curate.configure do |config|
           config.default_antivirus_instance = lambda {|path|
             '2'
           }
         end
-
         expect(Curate.configuration.default_antivirus_instance.call(__FILE__)).
           to eq("2")
       end
