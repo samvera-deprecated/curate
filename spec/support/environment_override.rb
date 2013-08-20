02d36853a64aa6451d7a2ba5133b5b2ca5290279
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-20 12:31:58 -0400

    Tweaking anti-virus environment override help

--- spec/support/environment_override.rb
+++ spec/support/environment_override.rb
@@ -1,17 +1,11 @@
 module EnvironmentOverride
   module_function
   def with_anti_virus_scanner(success = true)
-    if ! Rails.configuration.respond_to?(:default_antivirus_instance)
-      raise RuntimeError, "Undefined Rails.configuration.default_antivirus_instance"
-    end
-    before_override = Rails.configuration.default_antivirus_instance
-
+    before_override = Curate.configuration.default_antivirus_instance
     instance = success ? lambda {|f| 0 } : lambda {|f| 1 }
-    Rails.configuration.default_antivirus_instance = instance
+    Curate.configuration.default_antivirus_instance = instance
     yield
   ensure
-    if Rails.configuration.respond_to?(:default_antivirus_instance)
-      Rails.configuration.default_antivirus_instance = before_override
-    end
+    Curate.configuration.default_antivirus_instance = before_override
   end
 end
