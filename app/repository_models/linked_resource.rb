3f1e276ed6363e74d534250a9dbadae103413e44
Justin Coyne <justin@curationexperts.com>
2013-08-22 14:29:02 -0500

    Associate an external item with a work. Fixes #61

---
+++ app/repository_models/linked_resource.rb
@@ -0,0 +1,32 @@
+class LinkedResource < ActiveFedora::Base
+  include Curate::ActiveModelAdaptor
+  include Sufia::ModelMethods
+
+  has_file_datastream "content", control_group: 'E'
+  belongs_to :batch, property: :is_part_of, class_name: 'ActiveFedora::Base'
+  has_metadata "descMetadata", type: GenericFileRdfDatastream
+
+  delegate_to :descMetadata, [ :date_uploaded, :date_modified, :creator], unique: true
+
+  validates :batch, presence: true
+
+  class_attribute :human_readable_short_description
+  self.human_readable_short_description = "An arbitrary single file."
+
+  def url= url
+    content.dsLocation = url
+  end
+
+  def url
+    content.dsLocation
+  end
+
+  def to_s
+    url
+  end
+
+  def human_readable_type
+    self.class.to_s.demodulize.titleize
+  end
+end
+
--- spec/factories/create_curation_concern.rb
+++ spec/factories/create_curation_concern.rb
@@ -1,11 +1,3 @@
 def FactoryGirl.create_curation_concern(factory_name, user, override_attributes = {})
-  object = FactoryGirl.build(factory_name, override_attributes)
-
-  object.apply_depositor_metadata(user.user_key)
-  object.creator = user.to_s
-  object.date_uploaded = Date.today
-  object.date_modified = Date.today
-  object.set_visibility(override_attributes[:visibility] || AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED)
-  object.save!
-  object
+  FactoryGirl.create(factory_name, override_attributes.merge(user: user))
 end
