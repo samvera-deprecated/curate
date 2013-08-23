0086d073808b46a8608ff135f200e39ae96d5cea
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-23 14:14:06 -0400

    Updating generator to stub Catalog
    Instead of deleting the CatalogController, I want the generatore (i.e.
    `rails g curate`) to create a app/controllers/catalog_controller.rb
    that requires Curate's catalog controller. It can then be overriden
    as required.
    
    Closes ndlib/planning#88
    Addresses curationexperts/curate_dce#2

---
+++ lib/generators/curate/templates/catalog_controller.rb
@@ -0,0 +1,6 @@
+# Generated via `rails generate curate`",
+require Curate::Engine.root.join('app/controllers/catalog_controller.rb')
+
+# If you want to re-open CatalogController uncomment the following lines:
+# class CatalogController
+# end
