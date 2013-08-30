01632f9a4eccef2ba92d1279ee72e3f48c4b5237
Justin Coyne <justin@curationexperts.com>
2013-08-30 10:56:40 -0500

    Sanitize linked resource urls. Fixes ndlib/curate#133

--- app/repository_models/linked_resource.rb
+++ app/repository_models/linked_resource.rb
@@ -12,9 +12,12 @@ class LinkedResource < ActiveFedora::Base
 
   class_attribute :human_readable_short_description
   self.human_readable_short_description = "An arbitrary single file."
+  include ActionView::Helpers::SanitizeHelper
 
   def url= url
-    content.dsLocation = url
+    u = URI::Parser.new.parse(url)
+    return unless [URI::HTTP, URI::HTTPS, URI::FTP].include?(u.class)
+    content.dsLocation = u.to_s
   end
 
   def url
