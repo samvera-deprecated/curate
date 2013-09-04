9a1cdf968491eaa69fd93aab1951254e34ab52aa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-04 16:01:33 -0400

    Replacing :actor, :curation_concern with config
    Instead of requiring the more archaic `register :actor` and `register
    :curation_concern` replace with a class attribute that defines the
    `curation_concern_type` and derives the parameter hash key from the
    `curation_concern_type`.
    
    Provide a shared example in which we can begin extracting default
    behavior of a controller.

--- spec/controllers/curation_concern/datasets_controller_spec.rb
+++ spec/controllers/curation_concern/datasets_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe CurationConcern::DatasetsController do
+  include_examples 'is_a_curation_concern_controller', Dataset
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }
 
