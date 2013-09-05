6214f5ec09ecd81aca8d9aa71b3d6720772f133e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-05 10:12:12 -0400

    Improving the curate:work generator
    The previous version was very spotty in what it provides, and while
    this is not the definitive version of the curate:work generator it
    does provide:
    
    * Controller and corresponding spec generation, highlighting that each
      work type concern is likely very similar to other works, but can
      easily be altered
    * Routes are now injected into an existing application
    * Added to Curate's test suite is the run-time generation of a work
      model via the curate:work generator and most importantly testing
      that those generated specs actually work
    
    Further refactorings includes, but is not limited to:
    
    * Removing duplication concerning actor specs; Given that works are
      viewed as likely very similar, each actor should have a macro
      for that particular spec.
    
    Closes #156, #157

--- lib/generators/curate/work/work_generator.rb
+++ lib/generators/curate/work/work_generator.rb
@@ -5,24 +5,45 @@ class Curate::WorkGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
 
   desc "Create a Repository Model."
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
-  def create_repository_model
-    template "model.rb.erb", "app/repository_models/#{file_name}.rb"
+  # Why all of these antics with defining individual methods?
+  # Because I want the output of Curate::WorkGenerator to include all the processed files.
+  def create_model_spec
     template "model_spec.rb.erb", "spec/repository_models/#{file_name}_spec.rb"
   end
-
-  # def create_controller
-  #   template "controller.rb.erb", "app/controllers/curation_concern/#{plural_file_name}_actor.rb"
-  #   template "controller_spec.rb.erb", "spec/controllers/curation_concern/#{plural_file_name}_actor.rb"
-  # end
-
+  def create_model
+    template("model.rb.erb", "app/repository_models/#{file_name}.rb")
+  end
+  def create_controller_spec
+    template("controller_spec.rb.erb", "spec/controllers/curation_concern/#{plural_file_name}_controller_spec.rb")
+  end
+  def create_actor_spec
+    template("actor_spec.rb.erb", "spec/services/curation_concern/#{file_name}_actor_spec.rb")
+  end
+  def create_factory
+    template("factory.rb.erb", "spec/factories/#{plural_file_name}_factory.rb")
+  end
+  def create_datastream
+    template("datastream.rb.erb", "app/repository_datastreams/#{file_name}_rdf_datastream.rb")
+  end
+  def create_controller
+    template("controller.rb.erb", "app/controllers/curation_concern/#{plural_file_name}_controller.rb")
+  end
   def create_actor
-    template "actor.rb.erb", "app/services/curation_concern/#{file_name}_actor.rb"
-    template "actor_spec.rb.erb", "spec/services/curation_concern/#{file_name}_actor.rb"
+    template("actor.rb.erb", "app/services/curation_concern/#{file_name}_actor.rb")
   end
 
-  def create_factory
-    template "factory.rb.erb", "spec/factories/#{plural_file_name}_factory.rb"
+  def update_routes
+    reg_exp = /^(\s+curate_for\(? *\{? *:?containers:? *=?\>? *\[)([^\]]*)(\] *\}? *\)?)\s*$/
+    gsub_file 'config/routes.rb', reg_exp do |match|
+      match =~ reg_exp
+      # Don't re-add the route
+      if !$2.include?(plural_table_name)
+        match = $1 << $2 << ", :#{plural_table_name}" << $3
+      end
+      match
+    end
   end
 
   # def create_views
