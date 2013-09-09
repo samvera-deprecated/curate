25a37311265461f757cb304b69132d767ac427f3
val99erie <val99erie@gmail.com>
2013-09-09 14:48:17 -0500

    Default value for GenericWork#rights.  Issue ndlib/planning#127

--- app/repository_models/generic_work.rb
+++ app/repository_models/generic_work.rb
@@ -14,6 +14,7 @@ class GenericWork < ActiveFedora::Base
 
   attribute :rights, datastream: :descMetadata,
     multiple: false,
+    default: Sufia.config.cc_licenses['All rights reserved'],
     validates: {presence: { message: 'You must select a license for your work.' }}
 
   attribute :created, datastream: :descMetadata, multiple: false
