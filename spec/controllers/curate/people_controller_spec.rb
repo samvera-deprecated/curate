a762a8d8121fdd5196ddfc27e9d34b102639a81f
val99erie <val99erie@gmail.com>
2013-09-10 16:01:47 -0500

    View for a profile.  Issue ndlib/planning#175

---
+++ spec/controllers/curate/people_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Curate::PeopleController do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:person) { user.person }
+  let(:a_different_user) { FactoryGirl.create(:user) }
+
+  describe "#show" do
+    context 'my own person page' do
+      before do
+        sign_in user
+        get :show, id: person.pid
+      end
+
+      it "should show me the page" do
+        expect(response).to be_success
+      end
+
+      it "assigns person" do
+        assigns(:person).should == person
+      end
+    end
+
+    context 'someone elses person page' do
+      before { sign_in a_different_user }
+
+      it "should show me the page" do
+        get :show, id: person.pid
+        expect(response).to be_success
+      end
+    end
+  end
+
+end
