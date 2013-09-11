f5dc7f91fcc117f894f4d0d89f09325dd839cf83
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-11 19:43:59 -0400

    Refactoring Curate::User
    Attempting to keep the concerns "mixable" in that we may have a
    curate instance in which the developers chooses don't want the
    person attributes

---
+++ app/models/curate/user/base.rb
@@ -0,0 +1,19 @@
+module Curate
+  module User
+    module Base
+
+      def agree_to_terms_of_service!
+        update_column(:agreed_to_terms_of_service, true)
+      end
+
+      def collections
+        Collection.where(Hydra.config[:permissions][:edit][:individual] => user_key)
+      end
+
+      def get_value_from_ldap(attribute)
+        # override
+      end
+
+    end
+  end
+end
