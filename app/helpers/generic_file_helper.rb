270729d50f846eeb69e677e3812c5ed17a735519
val99erie <val99erie@gmail.com>
2013-09-13 15:09:41 -0500

    Conditionally display file name.  Issue ndlib/planning#106

---
+++ app/helpers/generic_file_helper.rb
@@ -0,0 +1,11 @@
+module GenericFileHelper
+
+  def generic_file_title(gf)
+    can?(:read, gf) ? gf.to_s : "File"
+  end
+
+  def generic_file_link_name(gf)
+    can?(:read, gf) ? gf.filename : "File"
+  end
+
+end
