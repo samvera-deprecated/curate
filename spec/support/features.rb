42b2541d044c9152215886c7e7949a235913f897
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-23 11:26:50 -0400

    Cauterizing failing tests
    There are a handful of failing tests that need to be addressed. One is
    related to the alpha ActiveFedora 7. Another is related to a change
    in Blacklight/Kaminari. And third, we have a preferred email entry
    that is naive in its implementation (i.e. preferred email should simply
    be email and removed from the form as it is confusing)

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../features/session_helpers', __FILE__)
 require File.expand_path('../features/javascript', __FILE__)
 require File.expand_path('../features/create_works', __FILE__)
+require File.expand_path('../curate_fixture_file_upload', __FILE__)
 
 RSpec.configure do |config|
   config.include CurateFixtureFileUpload
