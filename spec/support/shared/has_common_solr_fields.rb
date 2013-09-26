98d1f4b2872d011a6b3496057cd1611923f5edb9
Justin Coyne <justin@curationexperts.com>
2013-09-26 11:07:47 -0500

    Switch from using archived_object_type to using resource_type so that Collection uses the same property as Works and Person
    Using a new solr field generic_type_sim to store the generic type
    (e.g. Collection, Work, Person)

--- spec/support/shared/has_common_solr_fields.rb
+++ spec/support/shared/has_common_solr_fields.rb
@@ -2,9 +2,9 @@ shared_examples 'has_common_solr_fields' do
   let(:solr_doc) { subject.to_solr}
 
   it "should index concrete an abstract type" do
-    subject.save(validate: false) # save causes a callback which sets archived_object_type
-    solr_doc['desc_metadata__archived_object_type_sim'].should == [subject.human_readable_type]
-    solr_doc['desc_metadata__resource_type_sim'].should == ['Work']
+    subject.save(validate: false) # save causes a callback which sets resource_type
+    solr_doc['desc_metadata__resource_type_sim'].should == [subject.human_readable_type]
+    solr_doc['generic_type_sim'].should == ['Work']
   end
 
 end
