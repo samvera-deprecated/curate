41d479d7eac4b18f2bd4dc7c3e471889f00ab47d
Don Brower <dbrower@nd.edu>
2013-10-04 17:33:43 -0400

    Change callback names to not shadow each other.

--- app/repository_models/curation_concern/with_generic_files.rb
+++ app/repository_models/curation_concern/with_generic_files.rb
@@ -5,10 +5,10 @@ module CurationConcern
     included do
       has_many :generic_files, property: :is_part_of
 
-      after_destroy :after_destroy_cleanup
+      after_destroy :after_destroy_cleanup_generic_files
     end
 
-    def after_destroy_cleanup
+    def after_destroy_cleanup_generic_files
       generic_files.each(&:destroy)
     end
 
