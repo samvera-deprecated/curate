f06c0fefc05ed380b057cfa9dae2ef3b17680e47
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-14 13:02:45 -0400

    Tidying up controller specs
    Instead of having lots of custom code, controller specs now use the
    shared examples.
    
    Also ensuring that Images can have DOI assignment

--- spec/controllers/curation_concern/etds_controller_spec.rb
+++ spec/controllers/curation_concern/etds_controller_spec.rb
@@ -1,56 +1,5 @@
 require 'spec_helper'
 
 describe CurationConcern::EtdsController do
-  let(:user) { FactoryGirl.create(:user) }
-  before { sign_in user }
-
-  describe "#show" do
-    context "my own private work" do
-      let(:etd) { FactoryGirl.create(:private_etd, user: user) }
-      it "should show me the page" do
-        get :show, id: etd
-        expect(response).to be_success
-      end
-    end
-    context "someone elses private work" do
-      let(:etd) { FactoryGirl.create(:private_etd) }
-      it "should show 401 Unauthorized" do
-        get :show, id: etd
-        expect(response.status).to eq 401
-        response.should render_template(:unauthorized)
-      end
-    end
-    context "someone elses public work" do
-      let(:etd) { FactoryGirl.create(:public_etd) }
-      it "should show me the page" do
-        get :show, id: etd
-        expect(response).to be_success
-      end
-    end
-  end
-
-  describe "#create" do
-    it "should create a work" do
-      controller.curation_concern.stub(:persisted?).and_return(true)
-      controller.actor = double(:create => true)
-      post :create, accept_contributor_agreement: "accept"
-      response.should redirect_to curation_concern_etd_path(controller.curation_concern)
-    end
-  end
-
-  describe "#update" do
-    let(:etd) { FactoryGirl.create(:etd, user: user) }
-    it "should update the work " do
-      controller.actor = double(:update => true, :visibility_changed? => false)
-      patch :update, id: etd
-      response.should redirect_to curation_concern_etd_path(controller.curation_concern)
-    end
-    describe "changing rights" do
-      it "should prompt to change the files access" do
-        controller.actor = double(:update => true, :visibility_changed? => true)
-        patch :update, id: etd
-        response.should redirect_to confirm_curation_concern_permission_path(controller.curation_concern)
-      end
-    end
-  end
+  it_behaves_like 'is_a_curation_concern_controller', Etd, actions: :all
 end
