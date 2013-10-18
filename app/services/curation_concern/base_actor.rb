d73dbf21b2d0b1e7284dd4778c3a13d76696db02
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-18 13:02:59 -0400

    Refactoring actor for better clarity
    * Re-arranging BaseActor for better clarity
    * Removing bang! methods
    * Ensuring method instructions are at the same level of abstraction

--- app/services/curation_concern/base_actor.rb
+++ app/services/curation_concern/base_actor.rb
@@ -12,22 +12,34 @@ module CurationConcern
       @visibility = attributes[:visibility]
     end
 
-    def create!
-      apply_creation_data_to_curation_concern
-      save! && assign_remote_identifiers_if_applicable
-    end
+    attr_reader :visibility
+    protected :visibility
+
+    delegate :visibility_changed?, to: :curation_concern
 
     def create
       apply_creation_data_to_curation_concern
       save && assign_remote_identifiers_if_applicable
     end
 
+    def update
+      save
+    end
+
+    protected
     def apply_creation_data_to_curation_concern
-      curation_concern.apply_depositor_metadata(user.user_key)
+      apply_depositor_metadata
       apply_owner_metadata
+      apply_deposit_date
+    end
+
+    def apply_depositor_metadata
+      curation_concern.apply_depositor_metadata(user.user_key)
+    end
+
+    def apply_deposit_date
       curation_concern.date_uploaded = Date.today
     end
-    protected :apply_creation_data_to_curation_concern
 
     def assign_remote_identifiers_if_applicable
       Hydra::RemoteIdentifier.requested_remote_identifiers_for(curation_concern) do |remote_service|
@@ -35,64 +47,41 @@ module CurationConcern
       end
       true # included to make sure save chaining can occur
     end
-    protected :assign_remote_identifiers_if_applicable
-
-    def update!
-      save!
-    end
-
-    def update
-      save
-    end
-
-    def save!
-      apply_save_data_to_curation_concern
-      curation_concern.save!
-    end
-    protected :save!
 
     def save
       apply_save_data_to_curation_concern
       curation_concern.save
     end
-    protected :save
 
     def apply_save_data_to_curation_concern
       curation_concern.attributes = attributes
       curation_concern.date_modified = Date.today
     end
-    protected :apply_save_data_to_curation_concern
-
-    attr_reader :visibility
-    protected :visibility
-
-    delegate :visibility_changed?, to: :curation_concern
 
     def attach_file(generic_file, file_to_attach)
       ActiveSupport::Deprecation.warn("removing #{self.class}#attach_file, use CurationConcern.attach_file instead")
       CurationConcern.attach_file(generic_file, user, file_to_attach)
     end
-    protected :attach_file
 
-    protected
-      # Set the owner metadata field to the value supplied in the attributes if 
-      # the current user is allowed to deposit on behalf of the supplied owner.
-      # Grants edit access to the owner.
-      # This also deletes the owner key from the attributes so that it isn't 
-      # set again later when apply_save_data_to_curation_concern is called.
-      def apply_owner_metadata
-        owner = owner_from_attributes || user
-        curation_concern.edit_users += [owner.user_key]
-        curation_concern.owner = owner.user_key
-      end
 
-      def owner_from_attributes
-        owner = candidate_owner
-        owner if owner && user.can_make_deposits_for.include?(owner)
-      end
+    # Set the owner metadata field to the value supplied in the attributes if
+    # the current user is allowed to deposit on behalf of the supplied owner.
+    # Grants edit access to the owner.
+    # This also deletes the owner key from the attributes so that it isn't
+    # set again later when apply_save_data_to_curation_concern is called.
+    def apply_owner_metadata
+      owner = owner_from_attributes || user
+      curation_concern.edit_users += [owner.user_key]
+      curation_concern.owner = owner.user_key
+    end
 
-      def candidate_owner
-        attributes.has_key?('owner') && User.find_by_user_key(attributes.delete('owner'))
-      end
+    def owner_from_attributes
+      owner = candidate_owner
+      owner if owner && user.can_make_deposits_for.include?(owner)
+    end
+
+    def candidate_owner
+      attributes.has_key?('owner') && User.find_by_user_key(attributes.delete('owner'))
+    end
   end
 end
