312d81bbbe68acf4ea2726449c4772bf927ca68c
Rajesh Balekai <rbalekai@nd.edu>
2013-11-26 10:42:55 -0500

    Gravatar should be displayed as default thumbnail for Person
    Display only profile sections from profile.members
    
    User edit page should have cancle link which takes back to person_path
    
    Closes ndlib/planning#361
    
    Explicily set phantomjs version
    
    Remove duplicate assertion definition
    
    Expand Rails gem to be ~> 4.0.0
    
    Expand Rails gem to be ~> 4.0.0
    
    Expand Rails gem to be ~> 4.0.0

--- spec/views/catalog/_index_partials/_thumbnail_display.html.erb_spec.rb
+++ spec/views/catalog/_index_partials/_thumbnail_display.html.erb_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 describe 'catalog/_index_partials/_thumbnail_display' do
-  let(:document) { FactoryGirl.create(:generic_work, representative: '1234') }
-  before do
-    render partial: 'thumbnail_display', locals: { document: document }
+  context 'Person: ' do
+    context 'Gravatar Image: ' do
+      let(:person) { FactoryGirl.create(:person, representative: '1234') }
+      before do
+        person.stub(:representative_image_url).and_return('http://www.gravatar.com/avatar/998e373d9226f0ac08b7b52084f32ac6/?s=300')
+        render partial: 'thumbnail_display', locals: { document: person }
+      end
+      it 'should display gravatar as thumbnail' do
+        rendered.should include("http://www.gravatar.com/avatar/998e373d9226f0ac08b7b52084f32ac6/?s=300")
+      end
+    end
+
+    context 'Uploaded Image: ' do
+      let(:person) { FactoryGirl.create(:person, representative: '1234') }
+      before do
+        person.stub(:representative_image_url).and_return('/downloads/1234?datastream_id=thumbnail')
+        render partial: 'thumbnail_display', locals: { document: person }
+      end
+      it 'should display uploaded image as thumbnail' do
+        rendered.should include("/downloads/1234?datastream_id=thumbnail")
+      end
+    end
   end
-  it 'should display thumbnail' do
-    rendered.should include("/downloads/1234?datastream_id=thumbnail")
+
+  context 'Work: ' do
+    let(:document) { FactoryGirl.create(:generic_work, representative: '1234') }
+    before do
+      render partial: 'thumbnail_display', locals: { document: document }
+    end
+    it 'should display selected representative as thumbnail' do
+      rendered.should include("/downloads/1234?datastream_id=thumbnail")
+    end
   end
 end
