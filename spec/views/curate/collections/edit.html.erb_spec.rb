e4496d755f75a03d41b4a43dd89c312a74a782b0
Rajesh Balekai <rbalekai@nd.edu>
2013-11-27 11:42:09 -0500

    Cancel link in Collection edit view
    Closes ndlib/planning#337

--- spec/views/curate/collections/edit.html.erb_spec.rb
+++ spec/views/curate/collections/edit.html.erb_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 
 describe 'curate/collections/edit.html.erb' do
-  let(:collection) { FactoryGirl.build(:collection) }
-  before(:each) do
-    assign(:collection, collection)
-    render
+  context 'Collection' do
+    let(:collection) { FactoryGirl.build(:collection) }
+    before(:each) do
+      assign(:collection, collection)
+      render
+    end
+    it 'renders page header' do
+      expect(view.content_for(:page_header)).to have_tag('h1 .human_readable_type')
+      expect(rendered).to have_link('Cancel', href: root_path)
+    end
   end
-  it 'renders page header' do
-    expect(view.content_for(:page_header)).to have_tag('h1 .human_readable_type')
+
+  context 'Edit Collection' do
+    let(:collection) {FactoryGirl.build(:collection, title: 'Collection 1') }
+    before(:each) do
+      collection.save
+      assign(:collection, collection)
+      render
+    end
+    it 'should have Cancel link which takes to show view' do
+      expect(rendered).to have_link('Cancel', href: collection_path(collection))
+    end
   end
 end
