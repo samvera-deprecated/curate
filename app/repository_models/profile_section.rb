5d6c116ad79fa7e8687d38017be957066faca028
Rajesh Balekai <rbalekai@nd.edu>
2014-01-31 11:09:33 -0500

    Make use of CollectionModel.

--- app/repository_models/profile_section.rb
+++ app/repository_models/profile_section.rb
@@ -4,6 +4,7 @@ class ProfileSection < ActiveFedora::Base
   include CurationConcern::CollectionModel
 
   def can_be_member_of_collection?(collection)
+    return false if collection.is_a?(Organization)
     collection.is_a?(Profile)
   end
 end
