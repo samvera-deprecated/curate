fbda567888cfab5fe5e6a2bd1a5e5d354512cf61
Glen Horton <hortongn@ucmail.uc.edu>
2014-02-28 12:38:32 -0500

    Repository manager can discover everything
    Add discover, show, and read abilities to repository managers.
    Override the gated_discovery_filters method in Hydra access controls
    so that repository managers have no filters and can discover
    everything.
    
    HYDRASIR-262 #close

--- app/models/concerns/curate/ability.rb
+++ app/models/concerns/curate/ability.rb
@@ -8,7 +8,7 @@ module Curate
     def curate_permissions
       alias_action :confirm, :copy, :to => :update
       if current_user.manager?
-        can [:edit, :update, :destroy], :all
+        can [:discover, :show, :read, :edit, :update, :destroy], :all
         cannot [:edit, :update, :destroy], Person
         cannot [:edit, :update, :destroy], Profile do |p|
           p.pid != current_user.profile.pid
