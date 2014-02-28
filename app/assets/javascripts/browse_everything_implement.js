a3284e643a0287bca3008f63e43ad59e7ff60a12
Xiaoming Wang <xw5d@virginia.edu>
2014-02-28 13:26:01 -0500

    fix error for attaching multiple files
    Edit page should not show Browse button
    
    Add browse everything partial to Attach a File page
    
    adding cloud resources module to generic file as well
    
    removing cloud_resource module since this attribute wont be used anymore
    
    not using js to parse the result from browse_everything
    
    adding cloud resource model with attr that match  browse_everything gem result
    
    reworking on actor to ingest cloud resource
    
    hooking up cloud_resouces params and processing it.
    
    fixing issue related to google drive
    
    adding config for hydracurate account in cloud
    
    code refactor to merge cloud resource attributes to be used by actor
    
    removing duplicate extension on filename
    
    ability to upload cloud resource for generic file

--- app/assets/javascripts/browse_everything_implement.js
+++ app/assets/javascripts/browse_everything_implement.js
@@ -4,14 +4,6 @@
 		$('#browse').browseEverything()
 			.done(function(data) {
 				console.log("DONE!!!!")
-					var jsonData=JSON.stringify(data, null, 2)
-					var urls= new Array();
-					$.each($.parseJSON(jsonData), function(idx, obj) {
-						console.log(obj.url);
-						urls.push(obj.url);
-					});
-
-				$('.cloud_resource').val(urls.join('|'))
 				$('#status').html(data.length.toString() + " item(s) selected")
 			})
 			.cancel(function()   { window.alert('Canceled!') });
