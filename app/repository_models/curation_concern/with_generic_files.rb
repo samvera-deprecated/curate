66526163bbe4b29db440e01fd6f1de3b728e2423
Thomas Scherz <thomas.scherz@uc.edu>
2014-03-06 12:23:15 -0500

    Solves Rubydora reload problem.

--- app/repository_models/curation_concern/with_generic_files.rb
+++ app/repository_models/curation_concern/with_generic_files.rb
@@ -4,13 +4,17 @@ module CurationConcern
 
     included do
       has_many :generic_files, property: :is_part_of
-
-      after_destroy :after_destroy_cleanup_generic_files
+      before_destroy :before_destroy_cleanup_generic_files
     end
 
     def after_destroy_cleanup_generic_files
       generic_files.each(&:destroy)
     end
 
+    def before_destroy_cleanup_generic_files
+      generic_files.each(&:destroy)
+    end
+
   end
 end
+
--- spec/temp/spec_delete.rb
+++ spec/temp/spec_delete.rb
@@ -1,7 +1,7 @@
 describe "#destroy" do
   let(:work_to_be_deleted) { FactoryGirl.create(default_work_factory_name, user: user) }
   let(:generic_file) { FactoryGirl.create_generic_file(:work_to_be_deleted, user,
-      curate_fixture_file_upload('files/image.png', 'image/png', false)) {|g| g.visibility = visibility }
+      curate_fixture_file_upload('files/image.png', 'image/png', false)) {|g| g.visibility = visibility }}
   it "should delete the work" do
     delete :destroy, id: work_to_be_deleted
     expect { GenericWork.find(work_to_be_deleted.pid) }.to raise_error
@@ -14,4 +14,5 @@ describe "#destroy" do
     expect(response).to redirect_to(controller.polymorphic_path([:curation_concern, parent]))
   end
 end
+
 
