442d3e4b08975ecb366c305c54853795eda83c0b
Thomas Scherz <thomas.scherz@uc.edu>
2014-03-12 11:50:05 -0400

    Adds spec test for cloud_resource model

---
+++ spec/models/cloud_resource_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe Cloud_Resource do
+
+  let(:download_url)
+  let(:auth_header)
+  let(:expiration)
+  let(:curation_concern)
+  let(:user)
+  let(:param_value)
+  let(:resources_to_ingest)
+
+  describe 'class methods' do
+
+    describe 'download_url'
+
+      describe 'initialize' do
+      end
+
+      describe 'url_key' do
+      end
+
+      describe 'expire_key' do
+      end
+    end
+
+    describe 'authorize_key' do
+
+      describe 'authorize_key' do
+      end
+
+      describe 'file' do
+      end
+
+      describe 'web' do
+      end
+
+      describe 'assign_headers' do
+      end
+
+      def 'filename' do
+      end
+
+      describe 'download_content_from_host' do
+      end
+    end
+
+    describe 'curation_concern' do
+
+      describe 'initialize' do
+      end
+
+      describe 'param_key' do
+      end
+
+    end
+
+    describe 'param_value' do
+
+      describe 'process_cloud_resource' do
+      end
+
+    end
+
+  end
+end
+
