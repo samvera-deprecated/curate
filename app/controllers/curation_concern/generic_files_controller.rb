eeda410358d6fa35b5a8bc49803e09f33d980a41
Thomas Scherz <thomas.scherz@uc.edu>
2014-03-12 11:50:05 -0400

    Adds spec test for cloud_resource model
    adding spec for cloud resource
    
    adding spec for base controller to respond to cloud resource
    
    moving common helper methods to base controller
    
    more cloud resource spec
    
    defaulting file system to spec/support folder to run features
    
    feature to add cloud resource
    
    Adds generic_file_actor_spec
    
    adding feature to attach a cloud resource to existing work
    
    removing debug statements
    
    removing debug statements
    
    Updates generic_file_actor_spec to test for files attached from cloud.
    
    Check if only one cloud resource is chosen and  remove alert statement from js
    
    Add browse-everything link to new etds and images page
    
    [skip ci]
    
    Unit tests for download_content_from_host. A lot of stubbing and mocking was needed
    
    Conflicts:
    	app/controllers/curation_concern/generic_works_controller.rb
    	dump.rdb

--- app/controllers/curation_concern/generic_files_controller.rb
+++ app/controllers/curation_concern/generic_files_controller.rb
@@ -9,6 +9,7 @@ class CurationConcern::GenericFilesController < CurationConcern::BaseController
   end
 
   before_filter :parent
+  before_filter :cloud_resources_valid?, only: :create
   before_filter :authorize_edit_parent_rights!, except: [:show]
 
   self.excluded_actions_for_curation_concern_authorization = [:new, :create]
@@ -16,6 +17,20 @@ class CurationConcern::GenericFilesController < CurationConcern::BaseController
     (action_name == 'versions' || action_name == 'rollback') ? :edit : super
   end
   protected :action_name_for_authorization
+
+  def cloud_resources_valid?
+    if params.has_key?(:selected_files) && params[:selected_files].length>1
+      respond_with([:curation_concern, curation_concern]) do |wants|
+        wants.html {
+          flash.now[:error] = "Please select one cloud resource at a time."
+          render 'new', status: :unprocessable_entity
+        }
+      end
+      return false
+    end
+  end
+
+  protected :cloud_resources_valid?
 
   self.curation_concern_type = GenericFile
 
@@ -78,15 +93,4 @@ class CurationConcern::GenericFilesController < CurationConcern::BaseController
   register :actor do
     CurationConcern.actor(curation_concern, current_user, attributes_for_actor)
   end
-
-  def hash_key_for_curation_concern
-    curation_concern_type.name.underscore.to_sym
-  end
-
-  def attributes_for_actor
-    return params[hash_key_for_curation_concern] if cloud_resources_to_ingest.nil?
-    params[hash_key_for_curation_concern].merge!(:cloud_resources=>cloud_resources_to_ingest)
-  end
-
-
 end
