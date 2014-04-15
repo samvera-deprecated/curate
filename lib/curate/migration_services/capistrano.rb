ec21fc77883699a9915fdce37a9db96ddbb0fe97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-04-15 12:04:42 -0400

    Including MetadataNormalization cap task

--- lib/curate/migration_services/capistrano.rb
+++ lib/curate/migration_services/capistrano.rb
@@ -4,7 +4,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       # It would be nice if I could auto-generate this list, but there are
       # too many dependencies introduced in the migration services objects
       # so for now this will be how it is done.
-      ['RelatedContributor', 'DisplayName'].each do |namespace|
+      ['RelatedContributor', 'DisplayName', 'MetadataNormalization'].each do |namespace|
         desc "Run the #{namespace} migration service"
         task namespace.to_sym, roles: :app do
           command = "require 'curate/migration_services/runner'; Curate::MigrationServices.run(container_namespace: 'Curate::MigrationServices::MigrationContainers::#{namespace}');"
