32384f2b247d498a31e0132a6a67bf9a2e11a4e3
Banurekha <banurekha.l@gmail.com>
2014-04-15 11:31:02 -0400

    intercept 404 on thumbnail with default thumbnail

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -4,4 +4,21 @@ class DownloadsController < ApplicationController
   include Sufia::Noid # for normalize_identifier method
   include Hydra::Controller::DownloadBehavior
   prepend_before_filter :normalize_identifier, except: [:index, :new, :create]
+
+  protected
+
+  def render_404
+    if params.has_key?(:datastream_id) and params[:datastream_id] == "thumbnail"
+      send_default_image
+    else
+      super
+    end
+  end
+
+  # Send default thumbnail image
+  def send_default_image
+    image= ActionController::Base.helpers.asset_path("curate/default.png", type: :image)
+    redirect_to image
+  end
+
 end
