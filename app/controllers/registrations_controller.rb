fb501e8937bad4557abe1aa73400686b7b1a56bc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-04-16 13:43:07 -0400

    Adjusting registration controller
    In the case of a user editing their own account, we need to ask them
    to reauthenticate (as the password field is being rendered?).
    
    In the case where I am editing another user's account (i.e. repository
    manager), then I am not asking for the password of the account being
    edited.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < Devise::RegistrationsController
     if current_user.manager?
       manager_is_updating_a_user_registration(&block)
     else
-      curruent_user_is_updating_their_user_registration(&block)
+      current_user_is_updating_their_user_registration(&block)
     end
   end
 
@@ -21,22 +21,25 @@ class RegistrationsController < Devise::RegistrationsController
       user = User.find(params[:user][:id])
       self.resource = resource_class.to_adapter.get!(user)
       scrub_password_parameters_for_manager!
-      handle_update_response(skip_signin: true, &block)
+      update_status = resource.update_without_password(account_update_params)
+      handle_update_response(update_status: update_status, skip_signin: true, &block)
     else
       curruent_user_is_updating_their_user_registration(&block)
     end
   end
 
-  def curruent_user_is_updating_their_user_registration(&block)
+  def current_user_is_updating_their_user_registration(&block)
     user = send(:"current_#{resource_name}").to_key
     self.resource = resource_class.to_adapter.get!(user)
-    handle_update_response(skip_signin: false, &block)
+    update_status = resource.update_with_password(account_update_params)
+    handle_update_response(successful_update: update_status, skip_signin: false, &block)
   end
 
   def handle_update_response(options = {}, &block)
     skip_signin = options.fetch(:skip_signin) { false }
-    if resource.update_without_password(account_update_params)
-      yield(resource) if block_given
+    successful_update = options.fetch(:successful_update)
+    if successful_update
+      yield(resource) if block_given?
       if is_navigational_format?
         flash_key =
         if update_needs_confirmation?(resource, prev_unconfirmed_email)
