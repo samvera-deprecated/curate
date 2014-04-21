bccf8c5a2b19f0b4805a790dbb50dfd4459d961b
Rajesh Balekai <rbalekai@nd.edu>
2014-04-21 12:00:35 -0400

    Remove associations when a Group/Work is deleted

--- app/repository_models/curation_concern/work.rb
+++ app/repository_models/curation_concern/work.rb
@@ -12,6 +12,10 @@ module CurationConcern
     end
     include Hydra::AccessControls::Permissions
 
+    included do
+      before_destroy :clear_associations
+    end
+
     def to_solr(solr_doc={}, opts={})
       super(solr_doc, opts)
       Solrizer.set_field(solr_doc, 'generic_type', 'Work', :facetable)
@@ -47,12 +51,35 @@ module CurationConcern
 
     def remove_editor(editor)
       if( ( self.depositor != editor.depositor ) && ( self.editors.include?( editor ) ) )
-        self.editors.delete(editor)
-        self.edit_users = self.edit_users - [editor.depositor]
-        self.save!
-        editor.works.delete(self)
-        editor.save!
+        remove_candidate_editor(editor)
       end
+    end
+
+    private
+
+    def clear_associations
+      clear_editor_groups
+      clear_editors
+    end
+
+    def clear_editor_groups
+      self.editor_groups.each do |editor_group|
+        remove_editor_group(editor_group)
+      end
+    end
+
+    def clear_editors
+      self.editors.each do |editor|
+        remove_candidate_editor(editor)
+      end
+    end
+
+    def remove_candidate_editor(editor)
+      self.editors.delete(editor)
+      self.edit_users = self.edit_users - [editor.depositor]
+      self.save!
+      editor.works.delete(self)
+      editor.save!
     end
   end
 end
