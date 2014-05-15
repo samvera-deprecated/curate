ac0694dc12828c5e4ce60ecc8e332f9d32215b4f
Rajesh Balekai <rbalekai@nd.edu>
2014-05-15 09:22:00 -0400

    Make Abstract field searchable

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -146,6 +146,7 @@ class CatalogController < ApplicationController
       label_name = solr_name("desc_metadata__title", :stored_searchable, type: :string)
       contributor_name = solr_name("desc_metadata__contributor", :stored_searchable, type: :string)
       description_name = solr_name("desc_metadata__description", :stored_searchable, type: :string)
+      abstract_name = solr_name("desc_metadata__abstract", :stored_searchable, type: :string)
       creator_name = solr_name("desc_metadata__creator", :stored_searchable)
       publisher_name = solr_name("desc_metadata__publisher", :stored_searchable, type: :string)
       language_name = solr_name("desc_metadata__language", :stored_searchable, type: :string)
@@ -154,7 +155,7 @@ class CatalogController < ApplicationController
       subject_name = solr_name("desc_metadata__subject", :stored_searchable, type: :string)
       identifier_name = solr_name("desc_metadata__identifier", :stored_searchable, type: :string)
       field.solr_parameters = {
-        :qf => "#{title_name} noid_tsi file_format_tesim #{contributor_name} #{description_name} #{creator_name} #{publisher_name} #{language_name} #{collection_name} #{contributor_institution_name} #{subject_name} #{identifier_name}",
+        :qf => "#{title_name} noid_tsi file_format_tesim #{contributor_name} #{abstract_name} #{description_name} #{creator_name} #{publisher_name} #{language_name} #{collection_name} #{contributor_institution_name} #{subject_name} #{identifier_name}",
         :pf => "#{title_name}"
       }
     end
@@ -203,11 +204,23 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('description') do |field|
-      field.label = "Abstract or Summary"
+      field.label = "Description or Summary"
       field.solr_parameters = {
         :"spellcheck.dictionary" => "description"
       }
       solr_name = solr_name("desc_metadata__description", :stored_searchable, type: :string)
+      field.solr_local_parameters = {
+        :qf => solr_name,
+        :pf => solr_name
+      }
+    end
+
+    config.add_search_field('abstract') do |field|
+      field.label = "Abstract or Summary"
+      field.solr_parameters = {
+        :"spellcheck.dictionary" => "abstract"
+      }
+      solr_name = solr_name("desc_metadata__abstract", :stored_searchable, type: :string)
       field.solr_local_parameters = {
         :qf => solr_name,
         :pf => solr_name
