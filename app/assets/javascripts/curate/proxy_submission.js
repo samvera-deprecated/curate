13f60ffb036b9b71c1999c17c1c174284ef461fa
Rajesh Balekai <rbalekai@nd.edu>
2014-05-22 11:54:01 -0400

    Autofill creator field and not contributor when delegate dropdown is selected

--- app/assets/javascripts/curate/proxy_submission.js
+++ app/assets/javascripts/curate/proxy_submission.js
@@ -1,10 +1,10 @@
 /**
- * This Javascript/JQuery method is used for the population of the contributor field
+ * This Javascript/JQuery method is used for the population of the creator field
  * when a proxy user is making a deposit on behalf of someone else.  The method gets the selected
  * person on behalf of whom the proxy person is making the deposit and places that person's name in
- * a Contributor field and clicks the contributor Add button.
+ * a creator field and clicks the creator Add button.
  */
-function updateContributors(){
+function updateCreators(){
 
     // Get the selected owner name from the owner control.
     // If it is 'Myself', then pluck the name from the display name on the dropdown menu in the title bar of the page.
@@ -16,8 +16,8 @@ function updateContributors(){
     }
     else if (ownerName === "") { return; }
 
-    // Put that name into the "Add" Contributor control and force a click of the Add button.
-    // Note that the last Contributor control is always the one into which a new user is entered.
-    $('input[id$=_contributor]').last().val(ownerName);
-    $("div[class*=_contributor] .add").click();
+    // Put that name into the "Add" creator control and force a click of the Add button.
+    // Note that the last creator control is always the one into which a new user is entered.
+    $('input[id$=_creator]').last().val(ownerName);
+    $("div[class*=_creator] .add").click();
 }
