8bf8b9425816bce9b6151c531d63ca98de40f0ec
James Van Mil <james.vanmil@gmail.com>
2014-09-04 12:22:15 -0400

    Update specs

--- spec/repository_models/dataset_spec.rb
+++ spec/repository_models/dataset_spec.rb
@@ -6,7 +6,6 @@ describe Dataset do
   it_behaves_like 'is_a_curation_concern_model'
   it_behaves_like 'with_related_works'
   it_behaves_like 'is_embargoable'
-  it_behaves_like 'has_dc_metadata'
   it_behaves_like 'has_common_solr_fields'
 
   ## it { should have_unique_field(:available) }
@@ -15,15 +14,15 @@ describe Dataset do
   it { should have_unique_field(:bibliographic_citation) }
   it { should have_multivalue_field(:coverage_spatial) }
   it { should have_multivalue_field(:coverage_temporal) }
-  it { should have_unique_field(:creator) }
+  it { should have_multivalue_field(:creator) }
   it { should have_unique_field(:date_created) }
   it { should have_unique_field(:date_modified) }
   it { should have_unique_field(:date_uploaded) }
-  it { should have_multivalue_field(:description) }
+  it { should have_unique_field(:description) }
   it { should have_unique_field(:doi) }
   it { should have_multivalue_field(:identifier) }
   it { should have_multivalue_field(:language) }
-  it { should have_multivalue_field(:note) }
+  it { should have_unique_field(:note) }
   it { should have_unique_field(:publisher) }
   it { should have_unique_field(:publisher_digital) }
   it { should have_unique_field(:requires) }
