abee2306787a2d2ba64ac3fc8d874eb58932f3f0
Rajesh Balekai <rbalekai@nd.edu>
2014-09-04 12:02:36 -0400

    correct typo in javascript

--- app/assets/javascripts/curate/content_submission.js
+++ app/assets/javascripts/curate/content_submission.js
@@ -13,11 +13,11 @@ function showHideLicenseText()
    if($license_text == "Independently Licensed"){
       $("#self_deposit").hide();
       $("#independent_license").show();
-      $("#article_license").attr('required', 'true');
+      $("[id$=license]").attr('required', 'true');
     }else{
       $("#independent_license").hide();
       $("#self_deposit").show();
-      $("#article_license").removeAttr('required');
+      $("[id$=license]").removeAttr('required');
     }
 
 }
